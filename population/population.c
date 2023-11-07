c25e5d93c2d10fa3ed7944b2a3d2b89f09b43e36
xybelle <150109343+xybelle@users.noreply.github.com>
2023-11-07 05:35:37 +0000

    Tue, Nov 7, 2023, 5:35 AM +00:00

--- population/population.c
+++ population/population.c
@@ -4,10 +4,23 @@
 int main(void)
 {
     // TODO: Prompt for start size
-    
+    int n;
+    do
+    {
+        n = get_int("Starting population size: ");
+    }
+    while (n <= 9);
+
     // TODO: Prompt for end size
+    int t;
+    do
+    {
+        t = get_int("Ending population size: ");
+    }
+    while (t <= n)
 
     // TODO: Calculate number of years until we reach threshold
+
 
     // TODO: Print number of years
 }
