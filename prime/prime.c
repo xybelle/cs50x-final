6f56e99f7744244662716c1a0128208e0b30056c
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-19 16:43:47 +0000

    Wed, Dec 20, 2023, 12:43 AM +08:00

--- prime/prime.c
+++ prime/prime.c
@@ -30,10 +30,17 @@ int main(void)
 
 bool prime(int number)
 {
-    for (int n = 2; n <= number; n++)
+    if (number == 2 || number == 3)
+        return true;
+
+    if (number <= 1 || number % 2 == 0 || number % 3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= number; i += 6)
     {
-        if (number % n == 0)
+        if (number % i == 0 || number % (i + 2) == 0)
             return false;
     }
+
     return true;
 }
