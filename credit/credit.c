509acdbac1da2bd26d255ae988bddeb686a512bf
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-20 16:13:47 +0000

    Thu, Dec 21, 2023, 12:13 AM +08:00

--- credit/credit.c
+++ credit/credit.c
@@ -8,6 +8,9 @@ int main(void)
     // Prompt for input
     long card = get_long("Card number: ");
     long ln = card % 10;
+    long a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p;
+
+    p = 
 
     printf("%li\n", ln);
 
