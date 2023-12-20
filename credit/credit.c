7b61d51820833cd067cf752e9c51b7369fd30f5c
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-20 19:22:25 +0000

    Thu, Dec 21, 2023, 3:22 AM +08:00

--- credit/credit.c
+++ credit/credit.c
@@ -12,65 +12,63 @@ int main(void)
     // Calculate checksum
     if (validity_checker(card))
     {
-        print_
-    }
-    long lastnum = calculate_last(card);
+        long lastnum = calculate_last(card);
 
-    // Check starting digits
-    long firsttwo;
-    scanf("%li", &card);
+        // Check starting digits
+        long firsttwo;
+        scanf("%li", &card);
 
-    firsttwo = card;
+        firsttwo = card;
 
-    while (firsttwo >= 100)
-    {
-        firsttwo = firsttwo / 10;
-    }
-    printf("first digit = %li\n", firsttwo);
-
-    // Check length
-    long le, count = 0;
-
-    le = card;
-    do
-    {
-        le /= 10;
-        count++;
-    }
-    while (le != 0);
-
-    printf("Number of digits: %li\n", count);
-
-    // Print card type and validity
-    if (40 <= firsttwo && firsttwo <= 49)
-    {
-        if (count == 13)
+        while (firsttwo >= 100)
         {
-            printf("VISA\n");
+            firsttwo = firsttwo / 10;
         }
-        else if (count == 16)
+        printf("first digit = %li\n", firsttwo);
+
+        // Check length
+        long le, count = 0;
+
+        le = card;
+        do
         {
-            printf("VISA\n");
-        }
-    }
-    else if (firsttwo == 34 || firsttwo == 37)
-    {
-        if (count == 15)
+            le /= 10;
+            count++;
+         }
+        while (le != 0);
+
+        printf("Number of digits: %li\n", count);
+
+        // Print card type and validity
+        if (40 <= firsttwo && firsttwo <= 49)
         {
-            printf("AMEX\n");
+            if (count == 13)
+            {
+                printf("VISA\n");
+            }
+            else if (count == 16)
+            {
+                printf("VISA\n");
+            }
         }
-    }
-    else if (51 <= firsttwo && firsttwo <= 55)
-    {
-        if (count == 16)
+        else if (firsttwo == 34 || firsttwo == 37)
         {
-            printf("MasterCard\n");
+            if (count == 15)
+            {
+                printf("AMEX\n");
+            }
+        }
+        else if (51 <= firsttwo && firsttwo <= 55)
+        {
+            if (count == 16)
+            {
+                printf("MasterCard\n");
+            }
+        }
+        else
+        {
+            printf("Invalid\n");
         }
     }
-    else
-    {
-        printf("Invalid\n");
-    }
-}
 
 
