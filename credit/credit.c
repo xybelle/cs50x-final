1fbec07a2fe2e1be5f3fdde33fbf7b926bbe1624
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-20 17:28:35 +0000

    Thu, Dec 21, 2023, 1:28 AM +08:00

--- credit/credit.c
+++ credit/credit.c
@@ -40,7 +40,7 @@ int main(void)
 
 
     // Print card type and validity
-    if ((firsttwo >= 40) && le == 13 || le == 16)
+    if (firsttwo >= 40 && le !< 13)
     {
         printf("VISA\n");
     }
