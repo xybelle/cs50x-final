bc7db21750de78e42e6b0d37558a13ccdebce4f3
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-20 16:06:25 +0000

    Thu, Dec 21, 2023, 12:06 AM +08:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,16 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int last_number(int ln);
+
 
 int main(void)
 {
     // Prompt for input
     int card = get_long("Card number: ");
+    int ln = card % 10;
+    printf("%i\n", ln);
 
     // Get numbers to calculate checksum
-    int ln = last_number(card);
-    printf("%i\n", ln);
+
 
     // Check card length
 
@@ -21,9 +22,4 @@ int main(void)
     // Print card type and validity
 }
 
-int last_number(int ln)
-{
-    ln = ln % 10;
-    return ln;
-}
 
