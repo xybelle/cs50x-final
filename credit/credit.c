d0c126cf536da979931e1065a9e0604e1961b949
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-20 14:33:40 +0000

    Wed, Dec 20, 2023, 10:33 PM +08:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,12 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    long cc;
+    do
+    {
+        cc = get_long("Card number: ");
+    }
+    while (cc < 9999999999999999)
 }
