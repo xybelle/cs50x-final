88c05c086c6897c6eb30d57ee39083b739041334
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-20 15:06:56 +0000

    Wed, Dec 20, 2023, 11:06 PM +08:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,27 +3,9 @@
 
 int main(void)
 {
-    long cc = 0;
-    do
-    {
-        cc = get_long("Card number: ");
-    }
-    while (cc > 5699999999999999);
+    // Prompt for input
+    int card = get_card();
 
-    if (cc >= 4000000000000000 && cc <= 4999999999999999)
-    {
-        printf("Visa\n");
-    }
-    else if (cc >= 3400000000000000 && cc <= 3799999999999999)
-    {
-        printf("AMEX\n");
-    }
-    else if (cc >= 5100000000000000 && cc <= 5599999999999999)
-    {
-        printf("Mastercard\n");
-    }
-    else;
-    {
-        printf("Invalid\n");
-    }
+    // Calculate checksum
+    int checksum = calc_check()
 }
