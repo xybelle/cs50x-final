9a29c95685f686e2e3df7621aa6529c46c252444
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-20 13:56:11 +0000

    Wed, Dec 20, 2023, 9:56 PM +08:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,6 +3,34 @@
 
 int main(void)
 {
-    int 
+    int size, i, j, k, l;
+
+    do
+    {
+        size = get_int("Height: ");
+    }
+    while (size < 1 || size > 8);
+
+    for (i = 1; i <= size; i++)
+    {
+        for (j = i; j < size; j++)
+        {
+            printf(" ");
+        }
+
+        for (k = 1; k <= i; k++)
+        {
+            printf("#");
+        }
+
+        printf(" ");
+
+        for (l = j; l < size; l++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
 
 }
