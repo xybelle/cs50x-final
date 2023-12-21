4b7cfff627aeca1f614728b65176cd504d1b3636
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-21 01:16:53 +0000

    Thu, Dec 21, 2023, 9:16 AM +08:00

--- practice/week_2/uppercase.c
+++ practice/week_2/uppercase.c
@@ -9,14 +9,7 @@ int main(void)
     printf("After:  ");
     for (int i = 0; i < strlen(s); i++)
     {
-        if (islower(s[i]))
-        {
-            printf("%c", toupper(s[i]));
-        }
-        else
-        {
-            printf("%c", s[i]);
-        }
+        printf("%c", toupper(s[i]));
     }
     printf("\n");
 }
