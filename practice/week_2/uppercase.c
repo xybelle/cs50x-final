ff11f5044abe69240ccc113d57d6579d31a6445e
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-21 01:12:48 +0000

    Thu, Dec 21, 2023, 9:12 AM +08:00

--- practice/week_2/uppercase.c
+++ practice/week_2/uppercase.c
@@ -5,5 +5,13 @@
 
 int main(void)
 {
-
+    string name = get_string("Before: ");
+    printf("After:  ");
+    for (int i = 0; i < strlen(name); i++)
+    {
+        if (s[i] >= 'a' && s[i] <= 'z')
+        {
+            printf("%c", s)
+        }
+    }
 }
