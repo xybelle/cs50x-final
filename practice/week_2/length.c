9cd3c17de4636f8c46aeda1a236eae8ba10a6e5e
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-21 00:53:39 +0000

    Thu, Dec 21, 2023, 8:53 AM +08:00

--- practice/week_2/length.c
+++ practice/week_2/length.c
@@ -1,15 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     string name = get_string("What's your name? ");
 
-    int n = 0;
-    while (name[n] != '\0')
-    {
-        n++;
-    }
-
+    int n = strlen(name);
     printf("%i\n", n);
 }
