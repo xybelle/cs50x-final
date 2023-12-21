3b9f3b05e60ead7403724e702aacaa3dffff37f1
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-21 00:49:47 +0000

    Thu, Dec 21, 2023, 8:49 AM +08:00

--- practice/week_2/length.c
+++ practice/week_2/length.c
@@ -3,5 +3,13 @@
 
 int main(void)
 {
-    
+    string name = get_string("What's your name? ");
+
+    int n = 0;
+    while (name[n] != '\0')
+    {
+        n++;
+    }
+
+    printf("%i\n", n);
 }
