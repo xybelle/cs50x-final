aef31a9d7d549ece9c6f3e07c3090015833b3e60
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-22 22:52:19 +0000

    Sat, Dec 23, 2023, 6:52 AM +08:00

--- practice/week_2/practice.c
+++ practice/week_2/practice.c
@@ -1,10 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     string word = get_string("Word: ");
-    int length;
+    int length = strlen(word);
     int array[];
-    if (array[0] > array[1] ||)
+    for (int i = 0; i < length; i++)
+    {
+
+    }
+
+    do
+    {
+        
+    }
 }
