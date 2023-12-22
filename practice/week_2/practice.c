7b5ae14e0d19d43fa3b29a112b2b7197fe667553
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-22 23:18:07 +0000

    Sat, Dec 23, 2023, 7:18 AM +08:00

--- practice/week_2/practice.c
+++ practice/week_2/practice.c
@@ -7,10 +7,10 @@ int main(void)
     string word = get_string("Word: ");
     int length = strlen(word);
     int array[length];
-    
+
     for (int i = 0; i < length; i++)
     {
-        if (array[i] < array[i + 1])
+        if (array[i] > array[i + 1])
         {
             array[i] = array[i + 1];
             printf("Yes\n");
