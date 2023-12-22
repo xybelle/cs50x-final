143c99d9d4a8af34a2097229427018a22e1db4b7
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-22 17:37:21 +0000

    Sat, Dec 23, 2023, 1:37 AM +08:00

--- practice/week_2/random.c
+++ practice/week_2/random.c
@@ -8,7 +8,18 @@ int add_two_ints(int a, int b);
 
 int main()
 {
-    // IDK
+    // ask user for input
+    printf("Give me an integer: ");
+    int x = GetInt();
+    printf("Give me another integer: ");
+    int y = GetInt();
+
+    // add the two numbers together via a function call
+    int z = add_two_ints(x, y);
+
+    // output the result
+    printf("The sum of %i and %i is %i\n", x, y, z);
+
 }
 
 float mult_two_numbers(float a, float b)
