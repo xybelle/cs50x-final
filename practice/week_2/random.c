62998f093e4a23684b554b6de9b611cb7bbd93ee
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-22 18:07:01 +0000

    Sat, Dec 23, 2023, 2:07 AM +08:00

--- practice/week_2/random.c
+++ practice/week_2/random.c
@@ -1,10 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
 
+bool valid_triangle(int z);
+
 int main(void)
 {
-    for (i = 0, i < 3, i ++)
+    int x[3];
+    for (int i = 0, i < 3, i ++)
     {
         int x = get_int("Give me a positive number: ");
     }
+
+    int y = valid_triangle(x[]);
+    if (y = 0);
+    {
+        printf("The numbers would form a triangle.\n");
+    }
+    else
+    {
+        printf("The numbers would not form a triangle.\n:);
+    }
 }
+
+bool valid_triangle(int z)
+{
+    if (int sum = x[0] +
+}
+
