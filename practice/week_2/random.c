43bac0cd76f6ce860318c5258d8862d98af81b85
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-22 17:59:07 +0000

    Sat, Dec 23, 2023, 1:59 AM +08:00

--- practice/week_2/random.c
+++ practice/week_2/random.c
@@ -1,42 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
 
-// I don't know what I want to do here
-int get_int_a(void);
-int get_int_b(void);
-//float mult_two_numbers(float a, float b);
-int add_two_ints(int a, int b);
-
-int main()
+int main(void)
 {
-    int x = get_int_a();
-    int y = get_int_b();
-
-    // add the two numbers together via a function call
-    int z = add_two_ints(x, y);
-
-    // output the result
-    printf("The sum of %i and %i is %i.\n", x, y, z);
-
-}
-
-int get_int_a(void)
-{
-    int a = get_int("Give me an integer: ");
-    return a;
-}
-int get_int_b(void)
-{
-    int b = get_int("Give me another integer: ");
-    return b;
-}
-
-//float mult_two_numbers(float a, float b)
-//{
-//    return a * b;
-//}
-
-int add_two_ints(int a, int b)
-{
-    return a + b;
+    for (i = 0, i < 3, i ++)
+    {
+        int x = get_int("Give me a positive number: ");
+    }
 }
