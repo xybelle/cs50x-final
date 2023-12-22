16e8c808e32229b3afa9a1abff6dc1093b7af50a
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-22 22:57:42 +0000

    Sat, Dec 23, 2023, 6:57 AM +08:00

--- practice/week_2/practice.c
+++ practice/week_2/practice.c
@@ -9,9 +9,6 @@ int main(void)
     int array[length];
     for (int i = 0; i < length; i++)
     {
-        if (array[i] < array[i +1])
-        {
-            printf("Yes\n");
-        }
+        array[i] < array[i +1]);
     }
 }
