0ece642dbb77cdf501efaedb06943ca30520f0fc
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-22 17:32:51 +0000

    Sat, Dec 23, 2023, 1:32 AM +08:00

--- practice/week_2/random.c
+++ practice/week_2/random.c
@@ -1,12 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// I don't know what I want to do here
+
 float mult_two_numbers(float a, float b);
 int add_two_ints(int a, int b);
 
 int main()
 {
-    
+    // IDK
 }
 
 float mult_two_numbers(float a, float b)
