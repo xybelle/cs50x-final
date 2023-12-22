b4d2dc4a465a2ccc38e314d75ed33e0e9f2399ee
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-22 17:31:13 +0000

    Sat, Dec 23, 2023, 1:31 AM +08:00

---
+++ practice/week_2/random.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+
+float mult_two_numbers(float a, float b);
+int add_two_ints(int a, int b);
+
+int main()
+{
+    
+}
+
+float mult_two_numbers(float a, float b)
+{
+    return a * b;
+}
+
+int add_two_ints(int a, int b)
+{
+    return a + b;
+}
