42dfd9ddd9fa944b804007294bece93ff68841f5
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-26 22:32:06 +0000

    Wed, Dec 27, 2023, 6:32 AM +08:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -31,6 +31,9 @@ string replace(string r[], string argv[1]);
     r = tolower(r);
     for (int i = 0; i < n; i ++)
     {
-        if (r[i] != a ')
+        do
+        {
+            printf
+        }
     }
 }
