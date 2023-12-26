a772df38198c9377da96d00ee7914461137b4efc
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-26 23:54:45 +0000

    Wed, Dec 27, 2023, 7:54 AM +08:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -24,7 +24,7 @@ int main(int argc, string argv[])
 
 string replace(string argv[1])
 {
-    int n = strlen(r);
+    int n = strlen(argv[1]);
     for (int i = 0; i < n ; i++)
     {
         argv[1][i] = tolower(argv[1][i]);
