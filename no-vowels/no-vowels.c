35b81d752956d5da94018997218b729abe22a7e1
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-26 22:02:54 +0000

    Wed, Dec 27, 2023, 6:02 AM +08:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -7,15 +7,22 @@
 #include <stdio.h>
 
 // function to replace vowels with numbers
-string replace(string );
+string replace(string r);
 
 int main(int argc, string argv[])
 {
     if ((argc > 2) && (argc < 1))
     {
         printf("Error: please type ./no-vowels (word)\n");
+        return 1;
     }
-    return 1;
+    else
+    {
+        string r;
+        printf(r)
+    }
 
-    
+
 }
+
+string replace(string );
