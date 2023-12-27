b97d8e0ef9cab3f2aaed26ac05aaffd1c788f734
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-27 07:13:40 +0000

    Wed, Dec 27, 2023, 3:13 PM +08:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -25,7 +25,7 @@ int main(int argc, string argv[])
 string replace(string argv[1], char string)
 {
     int n = strlen(argv[1]);
-    char string;
+    char string[];
     for (int i = 0; i < n; i ++)
     {
         if ((argv[1][i] = 'a') || (argv[1][i] ='e') || (argv[1][i] ='i') || (argv[1][i] ='o'))
