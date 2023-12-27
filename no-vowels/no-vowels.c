ba4c3a955a746e9550d12d667e5b28093cb6dc88
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-27 06:25:15 +0000

    Wed, Dec 27, 2023, 2:25 PM +08:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -27,3 +27,39 @@ int main(int argc, string argv[])
     printf("\n");
 }
 
+string replace(string argv[1])
+{
+    int n = strlen(argv[1]), i = 0;
+    char c[n];
+    string word = c[n]
+    for (i < n ; i++)
+    {
+        argv[1][i] = tolower(argv[1][i]);
+        argv[1][i] = c[i];
+    }
+
+    for (i < n; i ++)
+    {
+        if ((c[i] != 'a') || (c[i] !='e') || (c[i] !='i') || (c[i] !='o'))
+        {
+            // return argv[1][i];
+            return c[i];
+        }
+        else
+        {
+            switch (c[i])
+            {
+                case 'a':
+                    return "6";
+                case 'e':
+                    return "3";
+                case 'i':
+                    return "1";
+                case 'o':
+                    return "0";
+            }
+        }
+    }
+    return argv[1];
+}
+
