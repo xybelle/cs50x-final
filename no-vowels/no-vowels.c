7c6fe52c0b021b12665354bd50e9117cd04ca012
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-27 00:29:06 +0000

    Wed, Dec 27, 2023, 8:29 AM +08:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -33,10 +33,11 @@ string replace(string argv[1])
 
     for (int i = 0; i < n; i ++)
     {
-        if ((argv[1][i] != 'a') || (argv[1][i] !='e') || (argv[1][i] !='i') || (argv[1][i] !='o'))
+        if ((c[i] != 'a') || (c[i] !='e') || (c[i] !='i') || c[i] !='o'))
         {
             // return argv[1][i];
-            argv[1][i + 1];
+            return c[i];
+            
         }
         else
         {
