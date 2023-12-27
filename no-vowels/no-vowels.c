7a919b42c202438f6142dec541d1ef64ab3f727f
xybelle <150109343+xybelle@users.noreply.github.com>
2023-12-27 07:33:38 +0000

    Wed, Dec 27, 2023, 3:33 PM +08:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -24,7 +24,7 @@ int main(int argc, string argv[])
     {
         if ((argv[1][i] = 'a') || (argv[1][i] ='e') || (argv[1][i] ='i') || (argv[1][i] ='o'))
         {
-            argv[1][i] = replace(argv[1]);
+            argv[1][i] = replace(&argv);
             printf("%c", argv[1][i]);
         }
         else
