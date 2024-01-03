37895423c435ec7172175a6905a3e1db24d2ccb8
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-03 14:35:09 +0000

    Wed, Jan 3, 2024, 10:35 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,6 +5,8 @@
 #include <string.h>
 
 int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
@@ -13,6 +15,8 @@ int main(void)
 
     // Count the number of letters, words, and sentences in the text
     int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
 
     // Compute the Coleman-Liau index
 
@@ -30,4 +34,9 @@ int count_letters(string text)
 int count_words(string text)
 {
     // Return the number of words in text
+}
+
+int count_sentences(string text)
+{
+    // Return the number of sentences in text
 }
