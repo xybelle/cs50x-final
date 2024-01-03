bb9b75eb0c65bc6283ab6f1b8cf2acbc62fdfa5c
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-03 14:32:59 +0000

    Wed, Jan 3, 2024, 10:32 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,12 +4,15 @@
 #include <stdio.h>
 #include <string.h>
 
+int count_letters(string text);
+
 int main(void)
 {
     // Prompt user for some text
     string text = get_string("Text: ");
 
     // Count the number of letters, words, and sentences in the text
+    int letters = count_letters(text);
 
     // Compute the Coleman-Liau index
 
@@ -21,4 +24,10 @@ int main(void)
 int count_letters(string text)
 {
     // Return the number of letters in text
+    // isalpha()
+}
+
+int count_words(string text)
+{
+    // Return the number of words in text
 }
