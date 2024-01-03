366afcf52b0e9732599e8e952ce78c1e949ad83c
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-03 14:28:00 +0000

    Wed, Jan 3, 2024, 10:28 PM +08:00

---
+++ readability/readability.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Prompt user for some text
+    string text = get_string("Text: ");
+
+    // Count the number of letters, words, and sentences in the text
+
+    int letters;
+    while 
+
+    // Compute the Coleman-Liau index
+
+    // Print the grade level
+    printf("\n");
+
+}
