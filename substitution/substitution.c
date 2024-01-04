2dfc5c1b65a7ef674a0b1bee05c2ae83806c4885
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-04 09:49:29 +0000

    Thu, Jan 4, 2024, 5:49 PM +08:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,5 +3,12 @@
 
 int main(int argc, string argv[])
 {
-
+    if ((argc <= 1) || (argc > 2))
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+    else if (key_count(argv))
 }
+
+int 
