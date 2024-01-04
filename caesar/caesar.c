5bab0c711aa653bc873840f9bb230e90083b4a68
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-04 09:26:46 +0000

    Thu, Jan 4, 2024, 5:26 PM +08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -10,7 +10,7 @@ char rotate (char c, int k);
 int main(int argc, string argv[])
 {
     // Check number of command-line argument
-    if ((argc <= 1 ) || (argc > 2))
+    if ((argc <= 1 ) || (argc > 2) || (isalpha(argc)))
     {
         printf("Usage: ./caesar key\n");
         return 1;
