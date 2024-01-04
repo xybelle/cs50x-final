207b4523c0129a2068d5210c036ac395a37bfead
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-04 06:18:21 +0000

    Thu, Jan 4, 2024, 2:18 PM +08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,10 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string k)
+int main(int argc, string argv[])
 {
     if (argc <= 1 || argc > 2)
     {
-        printf(")
+        printf("Usage: ./caesar key\n");
+    }
+    else
+    {
+        plaintext();
     }
 }
+
+
