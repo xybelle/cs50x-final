da1e50896d7cf9d4851000a1ef46b85ae0de7c3a
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-04 06:25:27 +0000

    Thu, Jan 4, 2024, 2:25 PM +08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,9 +3,10 @@
 
 int main(int argc, string argv[])
 {
-    if (argc <= 1 || argc > 2)
+    if ((argc <= 1 ) || (argc > 2))
     {
         printf("Usage: ./caesar key\n");
+        return 1;
     }
     if (isdigit(argv[1]))
     {
