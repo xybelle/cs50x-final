e3319b04e2b1313c966a9e770fa1b869926a011f
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-04 08:09:33 +0000

    Thu, Jan 4, 2024, 4:09 PM +08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -50,13 +50,9 @@ int main(int argc, string argv[])
 
 bool only_digits(string argv[1])
 {
-    int i = strlen(argv[1]);
-    for (int j = 0; j < i; j++)
+    if (isdigit(argv[1]))
     {
-        if (isdigit(argv[1][j]))
-        {
-            return true;
-        }
+        return true;
     }
     return false;
 }
