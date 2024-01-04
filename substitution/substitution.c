22b6d5727ca4a08feb951ce155e9514a4591e7a5
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-04 10:56:02 +0000

    Thu, Jan 4, 2024, 6:56 PM +08:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -7,6 +7,7 @@ bool all_unique(char c);
 int main(int argc, string argv[])
 {
     // Check the number of command-line argument
+    char key[26] = argv[1]
     if ((argc <= 1) || (argc > 2))
     {
         printf("Usage: ./substitution key\n");
@@ -15,12 +16,15 @@ int main(int argc, string argv[])
 
     // Check if key contains 26 characters
     int count = strlen(argv[1]);
-    if (count != 26)
+    if (count == 26)
+    {
+        all_alpha(argv[1]);
+    }
+    else
     {
         printf("Key must contain 26 characters.\n");
         return 1;
     }
-
     // Check if key contains anything but alphabet
     int digit = 0;
     for (int i = 0; i < count; i++)
@@ -68,3 +72,5 @@ bool all_unique(char c)
     }
     return true;
 }
+
+bool all_alpha(string card)
