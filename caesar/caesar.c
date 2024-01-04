e3f521833d0a0ab730a90fdcd9badd2947050cf5
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-04 06:32:13 +0000

    Thu, Jan 4, 2024, 2:32 PM +08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,24 +3,29 @@
 
 int main(int argc, string argv[])
 {
+    // Make sure program was run with just one commandline argument
     if ((argc <= 1 ) || (argc > 2))
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
+
+    // Make sure every character in arg[1] is a digit
     if (isdigit(argv[1]))
     {
         plaintext(argv[1]);
     }
+    // Convert argv[1] from a 'string' to an 'int'
     else if
     {
         conversion(argv[1]);
     }
 }
 
+// Prompt User for plaintext
 int plaintext(string argv[1], string plain)
 {
-    
+
 }
 
 
