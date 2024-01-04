9333a3499ee8f0b5200480ed8d626ae1567713a3
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-04 10:05:12 +0000

    Thu, Jan 4, 2024, 6:05 PM +08:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -11,14 +11,20 @@ int main(int argc, string argv[])
     }
 
     // Check if key contains 26 characters
-    int count = strlen(argv);
+    int count = strlen(argv[1]);
     if (count != 26)
     {
         printf("Key must contain 26 characters.\n");
     }
 
     // Check if key contains anything but alphabet
-    
+    do
+    {
+        if (isalnum(argv[1]))
+        {
+            
+        }
+    }
 
     // Check if each key is unique
 
