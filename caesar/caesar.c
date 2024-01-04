bb9238b9d3a10bcc48482864b38039a41fe6b1a6
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-04 08:46:47 +0000

    Thu, Jan 4, 2024, 4:46 PM +08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -36,7 +36,7 @@ int main(int argc, string argv[])
         printf("Ciphertext: ");
         for (int j = 0; j < i; j++)
         {
-            printf("%i ", plaintext[j]);
+            printf("%c ", plaintext[j]);
         }
         printf("\n");
 
