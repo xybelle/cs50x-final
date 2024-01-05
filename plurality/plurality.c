76747003ae1435ee28faa0981220ce043a103aba
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-05 14:44:45 +0000

    Fri, Jan 5, 2024, 10:44 PM +08:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -69,7 +69,7 @@ bool vote(string name)
     {
         if (strcmp(candidates[i].name, name) == 0)
         {
-            candidates[i].vote + 1;
+            candidates[i].votes++;
             return true;
         }
     }
