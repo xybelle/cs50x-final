43a6ed878d91189bdc1ec820f4cbaefa60ddaeff
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-05 15:07:05 +0000

    Fri, Jan 5, 2024, 11:07 PM +08:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -87,7 +87,7 @@ void print_winner(void)
     {
         if (candidates[i].votes) > candidates[i + 1].votes)
         {
-            
+            swap(candidates[i].name, candidates[i + 1].name)
         }
     }
     return candidates[].name;
