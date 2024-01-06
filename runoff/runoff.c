b37260d8338bd19b7b5cdb5777b74e5ecdd4f9e7
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-06 10:50:46 +0000

    Sat, Jan 6, 2024, 6:50 PM +08:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -192,7 +192,14 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    for ()
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if ((!candidates[i].eliminated) && (candidates[i].votes < min))
+        {
+            min = candidates[i].votes;
+        }
+        break;
+    }
     return false;
 }
 
