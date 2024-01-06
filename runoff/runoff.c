3e90de0128987ebf94626af972ae285ac04b2afd
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-06 12:26:17 +0000

    Sat, Jan 6, 2024, 8:26 PM +08:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -127,13 +127,13 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    for (voter = 0; voter < voter_count; voter++)
+    for (int i = 0; i < voter_count; i++)
     {
-        if (strcmp(candidates[voter].name, name) == 0)
+        if (strcmp(candidates[i].name, name) == 0)
         {
-            for (rank = 0; rank < candidate_count; rank++)
+            for (int j = 0; j < candidate_count; j++)
             {
-                preferences[voter][rank] = rank;
+                preferences[i][j] = rank;
             }
         }
     }
