0aacc857a2bfffd8487e0a3fc9c810c4a6dd8cb3
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-06 13:26:10 +0000

    Sat, Jan 6, 2024, 9:26 PM +08:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -131,7 +131,7 @@ bool vote(int voter, int rank, string name)
    {
         if (strcmp(candidates[i].name, name) == 0)
         {
-            preferences[voter][rank] = rank;
+            preferences[voter][rank] = i;
             return true;
         }
    }
