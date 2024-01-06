cd6ee34d1600d582cabb923a9c105708cee9c94d
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-06 16:24:39 +0000

    Sun, Jan 7, 2024, 12:24 AM +08:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -145,7 +145,7 @@ void tabulate(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (candidates[j].eliminated == false)
+            if (candidates[preferences[i][j]].eliminated == false)
             {
                 candidates[preferences[i][j]].votes++;
                 break;
