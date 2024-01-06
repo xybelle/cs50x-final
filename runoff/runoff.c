814c3d1eda86d77ce18fb247ba7e0a107fef8137
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-06 09:58:08 +0000

    Sat, Jan 6, 2024, 5:58 PM +08:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -166,7 +166,11 @@ bool print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes => ())
+        if (candidates[i].votes > (voter_count/2))
+        {
+            printf("%s\n", candidates[i].name);
+            return true;
+        }
     }
     return false;
 }
