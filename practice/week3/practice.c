9d5a5ab18d2a918258bff0f2ad38527176f7f3af
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-09 04:48:13 +0000

    Tue, Jan 9, 2024, 12:48 PM +08:00

--- practice/week3/practice.c
+++ practice/week3/practice.c
@@ -102,7 +102,7 @@ bool vote(int rank, string name, int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(name, candidates[i]))
+        if (strcmp(candidates[i], name) == 0)
         {
             ranks[rank] = i;
             return true;
