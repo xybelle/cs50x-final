e8084d148f7f7d68939c53e1cbbb49029b02f14a
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-09 13:37:24 +0000

    Tue, Jan 9, 2024, 9:37 PM +08:00

--- volume/volume.c
+++ volume/volume.c
@@ -42,9 +42,9 @@ int main(int argc, char *argv[])
         fwrite(&output, sizeof(header), 1, output);
     }
 
-    
-
     // TODO: Read samples from input file and write updated data to output file
+
+    
 
     // Close files
     fclose(input);
