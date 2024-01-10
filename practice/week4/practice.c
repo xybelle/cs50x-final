62971706d6753e77ac189c54896f8b73bc0b8c05
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-10 08:30:34 +0000

    Wed, Jan 10, 2024, 4:30 PM +08:00

--- practice/week4/practice.c
+++ practice/week4/practice.c
@@ -6,18 +6,10 @@ int main(void)
 {
     int rgbtRed, rgbtGreen, rgbtBlue;
     int sepiaRed = 255, sepiaGreen = 265, sepiaBlue = 165, max = 255;
-    if (sepiaRed <= 255  sepiaGreen <= 255 || sepiaBlue <= 255 )
-            {
-                rgbtRed = min(sepiaRed, max);
-                rgbtGreen = min(sepiaGreen, max);
-                rgbtBlue = min(sepiaBlue, max);
-            }
-            else
-            {
-                rgbtRed = 255;
-                rgbtGreen = 255;
-                rgbtBlue = 255;
-            }
+
+    rgbtRed = min(&sepiaRed, &max);
+    rgbtGreen = min(&sepiaGreen, &max);
+    rgbtBlue = min(&sepiaBlue, &max);
 
     printf("%i %i %i\n", rgbtRed, rgbtGreen, rgbtBlue);
 }
