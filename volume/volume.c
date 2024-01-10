601e61d3626afddb27c4cad02b925448084020cd
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-10 06:05:53 +0000

    Wed, Jan 10, 2024, 2:05 PM +08:00

--- volume/volume.c
+++ volume/volume.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
-    while (fread(&buffer, sizeof(int16_t), 1, input) != 0)
+    while (fread(&buffer, sizeof(int16_t), 1, input))
     {
         buffer *= factor;
         fwrite(&buffer, sizeof(int16_t), 1, output);
