30c3108ff3d9f3ed645a603a67a9bdb613038cf8
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-10 05:56:53 +0000

    Wed, Jan 10, 2024, 1:56 PM +08:00

--- volume/volume.c
+++ volume/volume.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
     while (fread(&buffer, sizeof(int16_t), 2, input) != 0)
     {
         buffer *= factor;
-        fwrite(&buffer, sizeof(int16_t), 1, output);
+        fwrite(&buffer, sizeof(int16_t), 2, output);
     }
 
 
