b22d8934a52becf341687d176dd6b71e0a30a95f
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-10 12:20:00 +0000

    Wed, Jan 10, 2024, 8:20 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -62,9 +62,9 @@ float min(float *a, int *b)
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     // Loop through all pixels
-    for (int i = 0; i < height/2; i++)
+    for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width/2; j++)
+        for (int j = 0; j < width; j++)
         {
             // Swap pixels
             swap(&image[i][j], &image[height][width]);
