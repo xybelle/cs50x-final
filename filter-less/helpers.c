b3b3a1b5e165932b2268f73d5aa5a3344067281a
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-10 13:27:11 +0000

    Wed, Jan 10, 2024, 9:27 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -94,7 +94,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 (i == height) && (j == width))
             {
                 // corner (4x)
-                image[i][j].rgbtRed = copy[i][j].rgbtRed + copy[i + 1][j + 1] / 
+                image[i][j].rgbtRed = copy[i][j].rgbtRed + copy[i][j + 1] + copy[i + 1][j] + copy[i + 1][j + 1];
                 image[i][j].rgbtGreen =
                 image[i][j].rgbtBlue =
             }
