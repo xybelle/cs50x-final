5bf83812f26627f14d7ae6e6de1d6e5162490f87
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-10 09:08:45 +0000

    Wed, Jan 10, 2024, 5:08 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -85,5 +85,13 @@ void swap(int *a, int *b)
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
     return;
 }
