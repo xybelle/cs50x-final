b43e0b30275023e14b609f83fd3fb09389fb88be
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-10 07:55:11 +0000

    Wed, Jan 10, 2024, 3:55 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -39,7 +39,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
                  + (.131 * image[i][j].rgbtBlue);
 
             // Update pixel values
-            if (sepiaRed || s)
+            if (sepiaRed || sepiaGreen || sepiaBlue )
             image[i][j].rgbtRed = sepiaRed;
             image[i][j].rgbtGreen = sepiaGreen;
             image[i][j].rgbtBlue = sepiaBlue;
