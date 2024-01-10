85d4e9ba8e04e0d6bfa6d0f915af432dc23186f1
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-10 07:51:18 +0000

    Wed, Jan 10, 2024, 3:51 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -24,14 +24,24 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    int sepiaRed, sepiaGreen, sepiaBlue;
     // Loop through all pixels
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             // Get sepia values
+            sepiaRed = (.393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen)
+                 + (.189 * image[i][j].rgbtBlue);
+            sepiaGreen = (.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen)
+                 + (.168 * image[i][j].rgbtBlue);
+            sepiaBlue = (.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen)
+                 + (.131 * image[i][j].rgbtBlue);
 
             // Update pixel values
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
         }
     }
     return;
