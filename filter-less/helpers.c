30d2da0688f5961d0db7244acf21a194d0d091da
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-11 07:35:04 +0000

    Thu, Jan 11, 2024, 3:35 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -78,32 +78,11 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 }
 
 // Swap pixels
-void swap(RGBTRIPLE *a, RGBTRIPLE *b[i][j])
+void swap(RGBTRIPLE *a, RGBTRIPLE *b)
 {
-    RGBTRIPLE tmp[0][0];
-    for (int i = 0; i < 1; i++)
-    {
-        for (int j = 0; j < 1; j++)
-        {
-            tmp = *a[i][j];
-        }
-    }
-
-    for (int i = 0; i < 1; i++)
-    {
-        for (int j = 0; j < 1; j++)
-        {
-            *a[i][j] = *b[i][j];
-        }
-    }
-
-    for (int i = 0; i < 1; i++)
-    {
-        for (int j = 0; j < 1; j++)
-        {
-            *b[i][j] = tmp[i][j];
-        }
-    }
+    RGBTRIPLE tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
 // Blur image
