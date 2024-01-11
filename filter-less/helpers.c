c891d3b79355ce0b082658d6ca8c3449ac7b3edf
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-11 07:25:16 +0000

    Thu, Jan 11, 2024, 3:25 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -88,6 +88,14 @@ void swap(RGBTRIPLE *a, RGBTRIPLE *b)
             tmp = *a[i][j];
         }
     }
+
+    for (int i = 0; i < 1; i++)
+    {
+        for (int j = 0; j < 1; j++)
+        {
+            *a[i][j] = *b[i][j];
+        }
+    }
     *a = *b;
     *b = tmp;
 }
