f44f19cd315c887442cdd3bc87f5a4834fbf7b52
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-12 07:54:37 +0000

    Fri, Jan 12, 2024, 3:54 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -66,7 +66,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     int h = height, w = width;
   //  RGBTRIPLE tmp[h][w];
     RGBTRIPLE *tmp[h][w];
-    tmp = malloc(h * w * sizeof(RGBTRIPLE));
+    tmp[h][w] = malloc(h * w * sizeof(RGBTRIPLE));
     if ( tmp == NULL)
     {
         return 1;
