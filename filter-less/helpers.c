c542f3019f7cad5cd3a662310c756cdd735d5c79
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-12 16:57:02 +0000

    Sat, Jan 13, 2024, 12:57 AM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -104,19 +104,79 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             float counter = 0.0, red = 0.0, green = 0.0, blue = 0.0;
-            for (int x = -1; x < 2; x++)
+            // Corners
+            if (((i == 0 && j == 0) || ((i == height - 1) && j == 0) || (i == 0 && (j == width - 1))
+                || ((i == height - 1) && (j == width - 1))))
             {
-                for (int y = -1; y < 2; y++)
+                for (int x = 0; x < 2; x++)
                 {
-                    red += copy[i + x][j + y].rgbtRed;
-                    green += copy[i + x][j + y].rgbtGreen;
-                    blue += copy[i + x][j + y].rgbtBlue;
-                    counter++;
+                    for (int y = 0; y < 2; y++)
+                    {
+                        red += copy[i + x][j + y].rgbtRed;
+                        green += copy[i + x][j + y].rgbtGreen;
+                        blue += copy[i + x][j + y].rgbtBlue;
+                        counter++;
+                    }
                 }
+                image[i][j].rgbtRed = round(red / (float) counter);
+                image[i][j].rgbtGreen = round(green / (float) counter);
+                image[i][j].rgbtBlue = round(blue / (float) counter);
             }
-            image[i][j].rgbtRed = round(red / (float) counter);
-            image[i][j].rgbtGreen = round(green / (float) counter);
-            image[i][j].rgbtBlue = round(blue / (float) counter);
+
+            // Left and right edges
+            if ( (j == 0 && ((i >= 1) && (i < height - 2))) || (j == width - 1 && ((i >= 1) && (i < height - 2))))
+            {
+                for (int x = -1; x < 2; x++)
+                {
+                    for (int y = 0; y <= 1; y++)
+                    {
+                        red += copy[i + x][j + y].rgbtRed;
+                        green += copy[i + x][j + y].rgbtGreen;
+                        blue += copy[i + x][j + y].rgbtBlue;
+                        counter++;
+                    }
+                }
+                image[i][j].rgbtRed = round(red / (float) counter);
+                image[i][j].rgbtGreen = round(green / (float) counter);
+                image[i][j].rgbtBlue = round(blue / (float) counter);
+            }
+
+            // Upper and lower edges
+            if ((i == 0 && ((j >= 1) && (j < width - 2))) || (i == height - 1 && ((j >= 1) && (j < width - 2))))
+            {
+                for (int x = 0; x <= 1; x++)
+                {
+                    for (int y = -1; y < 2; y++)
+                    {
+                        red += copy[i + x][j + y].rgbtRed;
+                        green += copy[i + x][j + y].rgbtGreen;
+                        blue += copy[i + x][j + y].rgbtBlue;
+                        counter++;
+                    }
+                }
+                image[i][j].rgbtRed = round(red / (float) counter);
+                image[i][j].rgbtGreen = round(green / (float) counter);
+                image[i][j].rgbtBlue = round(blue / (float) counter);
+            }
+
+            // Middle part
+            if ((i >= 1 && i <= (height - 2)) || (j >= 1 && j <= (width - 2)))
+            {
+
+                for (int x = -1; x < 2; x++)
+                {
+                    for (int y = -1; y < 2; y++)
+                    {
+                        red += copy[i + x][j + y].rgbtRed;
+                        green += copy[i + x][j + y].rgbtGreen;
+                        blue += copy[i + x][j + y].rgbtBlue;
+                        counter++;
+                    }
+                }
+                image[i][j].rgbtRed = round(red / (float) counter);
+                image[i][j].rgbtGreen = round(green / (float) counter);
+                image[i][j].rgbtBlue = round(blue / (float) counter);
+            }
         }
     }
     return;
