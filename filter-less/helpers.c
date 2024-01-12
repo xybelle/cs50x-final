b2175f30711970ade994b417b9ad84c28bc541ad
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-12 16:09:42 +0000

    Sat, Jan 13, 2024, 12:09 AM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -108,9 +108,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             if (((i == 0 && j == 0) || ((i == height - 1) && j == 0) || (i == 0 && (j == width - 1)) ||
                 ((i == height - 1) && (j == width - 1))))
             {
-                for (int x = -1; x < 2; x++)
+                for (int x = 0; x < 2; x++)
                 {
-                    for (int y = -1; y < 2; y++)
+                    for (int y = 0; y < 2; y++)
                     {
                         red += copy[i + x][j + y].rgbtRed;
                         green += copy[i + x][j + y].rgbtGreen;
