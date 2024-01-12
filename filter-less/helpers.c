f00197946645f91cd072fe235810599162ce97b3
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-12 09:22:12 +0000

    Fri, Jan 12, 2024, 5:22 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -74,7 +74,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0, n = width/2; j < n; j++)
+        for (int j = 0, n = width/2; j < width; j++)
         {
             image[i][j] = tmp[i][w];
             w--;
