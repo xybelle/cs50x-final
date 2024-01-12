57c3c5de8f2f2346224f1d47367981744358b6b1
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-12 16:55:05 +0000

    Sat, Jan 13, 2024, 12:55 AM +08:00

--- practice/week4/practice.c
+++ practice/week4/practice.c
@@ -37,47 +37,79 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            float counter = 0;
-
-            // Edge and corner
-            if (((i == 0 && j == 0) || ((i == height) && j == 0) || (i == 0 && (j == width)) || ((i == height) && (j == width))))
+            float counter = 0.0, red = 0.0, green = 0.0, blue = 0.0;
+            // Corners
+            if (((i == 0 && j == 0) || ((i == height - 1) && j == 0) || (i == 0 && (j == width - 1))
+                || ((i == height - 1) && (j == width - 1))))
             {
-                for (int x = -1; x < 2; x++)
+                for (int x = 0; x < 2; x++)
                 {
-                    for (int y = -1; y < 2; y++)
+                    for (int y = 0; y < 2; y++)
                     {
-                        copy[i][j].rgbtRed += copy[i + x][j + y].rgbtRed;
-                        copy[i][j].rgbtGreen += copy[i + x][j + y].rgbtGreen;
-                        copy[i][j].rgbtBlue += copy[i + x][j + y].rgbtBlue;
+                        red += copy[i + x][j + y].rgbtRed;
+                        green += copy[i + x][j + y].rgbtGreen;
+                        blue += copy[i + x][j + y].rgbtBlue;
                         counter++;
                     }
                 }
-                image[i][j].rgbtRed = round(copy[i][j].rgbtRed / (float) counter);
-                image[i][j].rgbtGreen = round(copy[i][j].rgbtGreen / (float) counter);
-                image[i][j].rgbtBlue = round(copy[i][j].rgbtBlue / (float) counter);
+                image[i][j].rgbtRed = round(red / (float) counter);
+                image[i][j].rgbtGreen = round(green / (float) counter);
+                image[i][j].rgbtBlue = round(blue / (float) counter);
+            }
 
-                break;
+            // Left and right edges
+            if ( (j == 0 && ((i >= 1) && (i < height - 2))) || (j == width - 1 && ((i >= 1) && (i < height - 2))))
+            {
+                for (int x = -1; x < 2; x++)
+                {
+                    for (int y = 0; y <= 1; y++)
+                    {
+                        red += copy[i + x][j + y].rgbtRed;
+                        green += copy[i + x][j + y].rgbtGreen;
+                        blue += copy[i + x][j + y].rgbtBlue;
+                        counter++;
+                    }
+                }
+                image[i][j].rgbtRed = round(red / (float) counter);
+                image[i][j].rgbtGreen = round(green / (float) counter);
+                image[i][j].rgbtBlue = round(blue / (float) counter);
+            }
+
+            // Upper and lower edges
+            if ((i == 0 && ((j >= 1) && (j < width - 2))) || (i == height - 1 && ((j >= 1) && (j < width - 2))))
+            {
+                for (int x = 0; x <= 1; x++)
+                {
+                    for (int y = -1; y < 2; y++)
+                    {
+                        red += copy[i + x][j + y].rgbtRed;
+                        green += copy[i + x][j + y].rgbtGreen;
+                        blue += copy[i + x][j + y].rgbtBlue;
+                        counter++;
+                    }
+                }
+                image[i][j].rgbtRed = round(red / (float) counter);
+                image[i][j].rgbtGreen = round(green / (float) counter);
+                image[i][j].rgbtBlue = round(blue / (float) counter);
             }
 
             // Middle part
-            if (((i > 1) && (i == (height - 1))) || ((j > 1) && (j == (width - 1))))
+            if ((i >= 1 && i <= (height - 2)) || (j >= 1 && j <= (width - 2)))
             {
 
                 for (int x = -1; x < 2; x++)
                 {
                     for (int y = -1; y < 2; y++)
                     {
-                        copy[i][j].rgbtRed += copy[i + x][j + y].rgbtRed;
-                        copy[i][j].rgbtGreen += copy[i + x][j + y].rgbtGreen;
-                        copy[i][j].rgbtBlue += copy[i + x][j + y].rgbtBlue;
+                        red += copy[i + x][j + y].rgbtRed;
+                        green += copy[i + x][j + y].rgbtGreen;
+                        blue += copy[i + x][j + y].rgbtBlue;
                         counter++;
                     }
                 }
-                image[i][j].rgbtRed = round(copy[i][j].rgbtRed / (float) counter);
-                image[i][j].rgbtGreen = round(copy[i][j].rgbtGreen / (float) counter);
-                image[i][j].rgbtBlue = round(copy[i][j].rgbtBlue / (float) counter);
-
-                break;
+                image[i][j].rgbtRed = round(red / (float) counter);
+                image[i][j].rgbtGreen = round(green / (float) counter);
+                image[i][j].rgbtBlue = round(blue / (float) counter);
             }
         }
     }
