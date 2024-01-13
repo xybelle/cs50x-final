e1134e6d313e6041980154f0621684007a630e8e
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-13 12:37:33 +0000

    Sat, Jan 13, 2024, 8:37 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -13,8 +13,17 @@ int main(int argc, char *argv[])
     }
 
     // Open memory card
-    FILE *file = fopen(argv[2], )
+    FILE *file = fopen(argv[2], "r");
+
     // Check if .raw - if not inform user and return 1
+    if (file == NULL)
+    {
+        printf("Cannot open %c\n", argv[2]);
+        return 1;
+    }
+
+    
+
     // Files generated should be named 000.jp for the first and counting up
     // Program if uses malloc must not leak memory
 
