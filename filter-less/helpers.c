46a7f3338731e1665df8e62e1cd763a709ee508d
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-13 12:13:22 +0000

    Sat, Jan 13, 2024, 8:13 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -109,7 +109,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             {
                 for (int y = -1; y < 2; y++)
                 {
-                    if (i + x < 0 || i + x > height || j + y < 0 || j + y > width)
+                    if (i + x < 0 || i + x >= height || j + y < 0 || j + y >= width)
                     {
                         continue;
                     }
