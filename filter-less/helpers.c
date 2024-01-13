39ae268ce9023334e412d6700ba6d93f41f912ad
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-13 12:09:06 +0000

    Sat, Jan 13, 2024, 8:09 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -105,18 +105,12 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             float counter = 0.0, red = 0.0, green = 0.0, blue = 0.0;
-            //bool skip = false;
             for (int x = -1; x < 2; x++)
             {
-                if (i + x < 0 || i + x > height)
-                {
-                    break;
-                }
                 for (int y = -1; y < 2; y++)
                 {
-                    if (j + y < 0 || j + y > width)
+                    if (i + x < 0 || i + x > height || j + y < 0 || j + y > width)
                     {
-                        //skip = true;
                         break;
                     }
                     red += copy[i + x][j + y].rgbtRed;
@@ -125,13 +119,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     counter++;
                 }
             }
-
-            //if (skip == false)
-
-                image[i][j].rgbtRed = round(red / (float) counter);
-                image[i][j].rgbtGreen = round(green / (float) counter);
-                image[i][j].rgbtBlue = round(blue / (float) counter);
-
+            image[i][j].rgbtRed = round(red / (float) counter);
+            image[i][j].rgbtGreen = round(green / (float) counter);
+            image[i][j].rgbtBlue = round(blue / (float) counter);
         }
     }
     return;
