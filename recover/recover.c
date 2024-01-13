45639a4c04d96c1427a8dcc778b04ad392d1db4e
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-13 12:29:01 +0000

    Sat, Jan 13, 2024, 8:29 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    // Check if 2 argv
+        // else print usage "Usage: ./recover image", then return 1
+    // Open memory card
+    // Check if .raw - if not inform user and return 1
+    // Files generated should be named 000.jp for the first and counting up
+    // Program if uses malloc must not leak memory
+
 }
