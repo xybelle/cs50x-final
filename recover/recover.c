6bdbbe112c0af99850e7390aede1a5e9b61a2289
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-13 15:42:00 +0000

    Sat, Jan 13, 2024, 11:42 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -23,17 +23,17 @@ int main(int argc, char *argv[])
 
     uint8_t buffer[BLOCK_SIZE];
     int count = 0;
-    char *filename = malloc(sizeof(char *));
-    if (filename == NULL)
-    {
-        return 3;
-    }
 
     // Read from memory card while there are still data left
     while (fread(buffer, sizeof(buffer), BLOCK_SIZE, card) == BLOCK_SIZE)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
+            char *filename = malloc(sizeof(char *));
+            if (filename == NULL)
+            {
+                return 3;
+            }
             sprintf(filename, "%03i.jpg", count);
 
             FILE *img = fopen(filename, "w");
@@ -47,10 +47,9 @@ int main(int argc, char *argv[])
             fwrite(buffer, sizeof(buffer), BLOCK_SIZE, img);
             count++;
             fclose(img);
+            free(filename);
         }
     }
-    
-    free(filename);
     fclose(card);
     return 0;
 }
