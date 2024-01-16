d81fdf8862fa26b3093ef7ba0f01164bc7f52cd0
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-16 12:10:13 +0000

    Tue, Jan 16, 2024, 8:10 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -28,6 +28,7 @@ int main(int argc, char *argv[])
     FILE *img = fopen(filename, "w");
     if (img == NULL)
     {
+        fclose(card);
         fclose(img);
         printf("Cannot create");
         return 4;
