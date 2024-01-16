254a5118bf6aa4b8485f72c3458c0347f409a8dd
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-16 08:51:45 +0000

    Tue, Jan 16, 2024, 4:51 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -23,14 +23,14 @@ int main(int argc, char *argv[])
 
     uint8_t buffer[BLOCK_SIZE];
     int count = 0;
-    char filename[8];
-    sprintf(filename, "%03i.jpg", count);
 
     // Read from memory card while there are still data left
     while (fread(buffer, sizeof(buffer), BLOCK_SIZE, card) == BLOCK_SIZE)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
+            char filename[8];
+            sprintf(filename, "%03i.jpg", count);
 
             FILE *img = fopen(filename, "w");
             if (img == NULL)
