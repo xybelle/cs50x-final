4fcd74b1e41f3087058daa19a99d53871302ab9d
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-16 12:33:58 +0000

    Tue, Jan 16, 2024, 8:33 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -32,20 +32,20 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            if (counter)
-
-            FILE *img = fopen(filename, "w");
-            if (img == NULL)
+            if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
             {
-                fclose(card);
-                fclose(img);
-                printf("Cannot create");
-                return 4;
+                FILE *img = fopen(filename, "w");
+                if (img == NULL)
+                {
+                    fclose(card);
+                    fclose(img);
+                    printf("Cannot create");
+                    return 4;
+                }
+                fwrite(buffer, sizeof(buffer), BLOCK_SIZE, img);
+                counter++;
             }
-            fwrite(buffer, sizeof(buffer), BLOCK_SIZE, img);
-            counter++;
-
-
+            else
             {
                 fclose(img);
             }
