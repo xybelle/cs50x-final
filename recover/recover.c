515b8ad313022b62296d98b0797331871abf37aa
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-16 08:57:11 +0000

    Tue, Jan 16, 2024, 4:57 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -23,22 +23,23 @@ int main(int argc, char *argv[])
 
     uint8_t buffer[BLOCK_SIZE];
     int count = 0;
+    char filename[8];
+    sprintf(filename, "%03i.jpg", count);
+
+    FILE *img = fopen(filename, "w");
+    if (img == NULL)
+    {
+        fclose(img);
+        printf("Cannot create");
+        return 4;
+    }
 
     // Read from memory card while there are still data left
     while (fread(buffer, sizeof(buffer), BLOCK_SIZE, card) == BLOCK_SIZE)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            char filename[8];
-            sprintf(filename, "%03i.jpg", count);
 
-            FILE *img = fopen(filename, "w");
-            if (img == NULL)
-            {
-                fclose(img);
-                printf("Cannot create");
-                return 4;
-            }
 
             fwrite(buffer, sizeof(buffer), BLOCK_SIZE, img);
             count++;
