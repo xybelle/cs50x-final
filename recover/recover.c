7ca0dba1ee9433a17d2ac05185e1325a5856b800
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-17 11:51:47 +0000

    Wed, Jan 17, 2024, 7:51 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -47,7 +47,6 @@ int main(int argc, char *argv[])
                 if (img == NULL)
                 {
                     fclose(card);
-                    fclose(img);
                     return 6;
                 }
 
