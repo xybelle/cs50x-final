302d5831609521e383933b56e3afdccbac81d692
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-19 06:58:08 +0000

    Fri, Jan 19, 2024, 2:58 PM +08:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -49,11 +49,11 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        person->parent0 = person;
-        person->parent1 = person;
+        person->parent[0] = parent0;
+        person->parent[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        
+
     }
 
     // If there are no generations left to create
