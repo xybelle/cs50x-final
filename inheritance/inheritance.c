9c835182c9a2573e0f7686b3b20ab42ff1293083
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-19 07:08:50 +0000

    Fri, Jan 19, 2024, 3:08 PM +08:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -39,7 +39,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    person *person = malloc(sizeof(person));
+    person *new_person = malloc(sizeof(person));
 
     // If there are still generations left to create
     if (generations > 1)
@@ -49,28 +49,28 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        person->parent[0] = parent0;
-        person->parent[1] = parent1;
+        new_person->parents[0] = parent0;
+        new_person->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        person->alleles[0] = parent0->alleles[rand() % 2];
-        person->alleles[1] = parent1->alleles[rand() % 2];
+        new_person->alleles[0] = parent0->alleles[rand() % 2];
+        new_person->alleles[1] = parent1->alleles[rand() % 2];
     }
 
     // If there are no generations left to create
     else
     {
         // TODO: Set parent pointers to NULL
-        person->parents[0] = NULL;
-        person->parents[1] = NULL;
+        new_person->parents[0] = NULL;
+        new_person->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-        person->alleles[0] = random_allele();
-        person->alleles[1] = random_allele();
+        new_person->alleles[0] = random_allele();
+        new_person->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
-    return person;
+    return new_person;
 }
 
 // Free `p` and all ancestors of `p`.
@@ -83,7 +83,7 @@ void free_family(person *p)
     }
 
     // TODO: Free parents recursively
-    free_family(p-parents[0]);
+    free_family(p->parents[0]);
     free_family(p->parents[1]);
 
     // TODO: Free child
