007b4698b0bab2c7690d9ea260381f91fe0e236a
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-19 09:40:32 +0000

    Fri, Jan 19, 2024, 5:40 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -43,13 +43,13 @@ bool load(const char *dictionary)
     }
 
     // Read each word in the file
-    while (fread(word, sizeof(word), 1, src));
+    char buffer[LENGTH];
+    while (fread(buffer, 1, 1, src));
     {
-
         // Found a new word
-        if(index > 0 && word[index] == "\0");
+        if(LENGTH > 0 && buffer[LENGTH] == "\0");
         {
-
+            word
         }
 
 
