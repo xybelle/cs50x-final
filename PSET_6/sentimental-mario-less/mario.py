7395e7dbbb02392f6df8ba0bc3e2d6bab429af8c
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-20 14:28:42 +0000

    Sat, Jan 20, 2024, 10:28 PM +08:00

--- PSET_6/sentimental-mario-less/mario.py
+++ PSET_6/sentimental-mario-less/mario.py
@@ -9,23 +9,12 @@ def main():
 
 
 def draw(h):
-    # If nothing to draw
-    if h <= 0:
-        return;
-
-    # Print pyramid of height h - 1
-    draw(h - 1)
-
-    # Print one more row
-    for _ in range(h):
-        print("#", end="")
-    print()
-
-main()
-
-
-def draw(h):
     for i in range(h):
         for j in range(h):
             print("*", end="")
-        for k in range(i)
+        for k in range(i):
+            print("#", end="")
+        print()
+
+
+main()
