c3443ba5ddba0119407eedf43ba306be9dcf3569
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-20 15:54:23 +0000

    Sat, Jan 20, 2024, 11:54 PM +08:00

---
+++ PSET_6/sentimental-readability/readability.py
@@ -0,0 +1,11 @@
+from cs50 import get_string
+
+
+def main()
+    txt = get_string("Text: ")
+
+    letters = count_letters(txt)
+    words = count_words(txt)
+    sentences = count_sentences(txt)
+
+    
