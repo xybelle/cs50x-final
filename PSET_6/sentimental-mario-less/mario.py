3c7a2a772bc04294e7db9f0b1ac0960e6ee3a1f0
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-20 13:29:26 +0000

    Sat, Jan 20, 2024, 9:29 PM +08:00

--- PSET_6/sentimental-mario-less/mario.py
+++ PSET_6/sentimental-mario-less/mario.py
@@ -1,3 +1,12 @@
 from cs50 import get_int
 
+def draw(n):
+    # Base case
+    if n == 1:
+        return 1
+    # Recursive call
+    else:
+        print("#" * draw(n - 1))
 
+height = get_int("Height: ")
+draw(height)
