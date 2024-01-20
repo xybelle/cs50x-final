8c0dd0a69f48b3df50ee33f40ca52ed80693fc44
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-20 14:36:50 +0000

    Sat, Jan 20, 2024, 10:36 PM +08:00

--- PSET_6/sentimental-mario-less/mario.py
+++ PSET_6/sentimental-mario-less/mario.py
@@ -10,7 +10,7 @@ def main():
 
 def draw(h):
     for i in range(h):
-        for j in range(h - k):
+        for j in range(h - i):
             print("*", end="")
         for k in range(i + 1):
             print("#", end="")
