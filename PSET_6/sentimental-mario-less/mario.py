544f36006d052ef80166575c0dd14ceb57e0992b
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-20 14:48:45 +0000

    Sat, Jan 20, 2024, 10:48 PM +08:00

--- PSET_6/sentimental-mario-less/mario.py
+++ PSET_6/sentimental-mario-less/mario.py
@@ -4,7 +4,7 @@ from cs50 import get_int
 def main():
     while True:
         height = get_int("Height: ")
-        if height > 0 and height < 8:
+        if 0 < height <= 8:
             break
     draw(height)
 
