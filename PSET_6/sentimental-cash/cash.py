f1a22d407ac5350dbd41afcf5648e2439387eac8
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-20 15:05:43 +0000

    Sat, Jan 20, 2024, 11:05 PM +08:00

---
+++ PSET_6/sentimental-cash/cash.py
@@ -0,0 +1,12 @@
+from cs50 import get_float
+
+def get_change():
+    while True
+        c = get_float("Change owed: ")
+        if c > 0:
+            return c
+        else:
+            pass
+
+def calc_quarters()
+    while 
