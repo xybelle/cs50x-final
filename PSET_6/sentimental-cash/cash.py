635bec57b46e4d0c54a4101a37204ae2f3567c9d
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-20 15:13:51 +0000

    Sat, Jan 20, 2024, 11:13 PM +08:00

--- PSET_6/sentimental-cash/cash.py
+++ PSET_6/sentimental-cash/cash.py
@@ -1,6 +1,25 @@
 from cs50 import get_float
 
-def get_change():
+
+def main()
+    cents = get_cents()
+    quarters = calc_quarters(cents)
+    cents = cents - quarters * 25
+
+    dimes = calc_dimes(cents)
+    cents = cents - dimes * 10
+
+    nickels = calc_nickels(cents)
+    cents = cents - nickels * 5
+
+    pennies = calc_pennies(cents)
+    cents = cents - pennies * 1
+
+    coins = quarters + dimes + nickels + pennies
+    print(coins)
+
+
+def get_cents():
     while True
         c = get_float("Change owed: ")
         if c > 0:
@@ -8,5 +27,33 @@ def get_change():
         else:
             pass
 
+
 def calc_quarters()
-    while 
+    while cents >= 25:
+        cents = -25
+        quarters += 1
+    return quarters
+
+
+def calc_dimes()
+    while cents >= 10:
+        cents = cents - 10
+        dimes += 1
+    return dimes
+
+
+def calc_nickels()
+    while cents >= 5:
+        cents = cents - 5
+        nickels += 1
+    return nickels
+
+
+def calc_pennies()
+    while cents >= 1
+        cents = cents - 1
+        pennies += 1
+    return pennies
+
+
+main()
