00ea04753b97880482bf1d2c05581c9683333d94
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-20 15:57:07 +0000

    Sat, Jan 20, 2024, 11:57 PM +08:00

--- PSET_6/sentimental-readability/readability.py
+++ PSET_6/sentimental-readability/readability.py
@@ -2,10 +2,19 @@ from cs50 import get_string
 
 
 def main()
+    # Prompt user for some text
     txt = get_string("Text: ")
 
+    # Compute total number of letters, words, and sentences in the text
     letters = count_letters(txt)
     words = count_words(txt)
     sentences = count_sentences(txt)
 
+    # Compute the Coleman-Liau index
+
+    # Print the grade level
+
+def count_letters(txt)
     
+
+
