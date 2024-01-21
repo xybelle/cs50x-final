86c3fc9baceca0f2745a955e8268733d57c08f1a
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-21 10:19:19 +0000

    Sun, Jan 21, 2024, 6:19 PM +08:00

--- PSET_6/dna/dna.py
+++ PSET_6/dna/dna.py
@@ -5,7 +5,7 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if sys.argc < 3:
+    if len(sys.argv) < 3:
         print("Usage ./dna FILE")
 
     # TODO: Read database file into a variable
