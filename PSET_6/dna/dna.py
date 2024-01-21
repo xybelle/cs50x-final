d78e252f7e67a5c22a56465fa4cb2683a3fa2f3b
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-21 09:01:00 +0000

    Sun, Jan 21, 2024, 5:01 PM +08:00

--- PSET_6/dna/dna.py
+++ PSET_6/dna/dna.py
@@ -17,10 +17,10 @@ def main():
 
     # TODO: Read DNA sequence file into a variable
     with open(sys.arg[2]) as sequence:
-        sequence.read()
+        read_data = sequence.read()
 
     # TODO: Find longest match of each STR in DNA sequence
-    
+
 
     # TODO: Check database for matching profiles
 
