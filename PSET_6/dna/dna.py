ec82942fe50d4d2befc9699a4a411c2fe3c457f1
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-22 11:46:41 +0000

    Mon, Jan 22, 2024, 7:46 PM +08:00

--- PSET_6/dna/dna.py
+++ PSET_6/dna/dna.py
@@ -29,14 +29,7 @@ def main():
 
     # TODO: Check database for matching profiles
     for i in range(len(dna_db)):
-        for i in range(len(keys_list[1:])):
-            match = []
-            if int(dna_db[f"{keys_list[i + 1]}"]) == int(subseq[f"{keys_list[i + 1]}"])
-                match = true
-            else
-                match = false
-
-        if match == true
+        if all
 
     #        print(f"{dna_db[i]['name']}")
         #else:
