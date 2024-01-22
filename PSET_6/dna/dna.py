97997c9e4f20dcea3d58350d3f104294bd3f9993
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-22 12:08:04 +0000

    Mon, Jan 22, 2024, 8:08 PM +08:00

--- PSET_6/dna/dna.py
+++ PSET_6/dna/dna.py
@@ -21,14 +21,9 @@ def main():
         seq = txtfile.read()
 
     # TODO: Find longest match of each STR in DNA sequence
+    subseq = dict.fromkeys(dna_db[0].keys())
     keys_list = list(dna_db[0].keys())
-    subseq = dict()
-
-    for i in range(len(keys_list[1:])):
-        subseq[f"{keys_list[i]}"] = 0
-
-    #subseq = dict.fromkeys(dna_db[0].keys())
-
+    
     for i in range(len(keys_list[1:])):
         subseq[f"{keys_list[i + 1]}"] = longest_match(seq, keys_list[i + 1])
 
