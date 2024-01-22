6c912a789adecd306fdfda7747be209b98b7c41b
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-22 13:01:33 +0000

    Mon, Jan 22, 2024, 9:01 PM +08:00

--- PSET_6/dna/dna.py
+++ PSET_6/dna/dna.py
@@ -34,7 +34,7 @@ def main():
     # TODO: Check database for matching profiles
     common_keys = set(dna_db[0].keys()) & set(subseq.keys())
     match = False
-    for row in dna_db:
+    for row in dna_db except row['name']:
         row_number = {key: int(value) for key, value in row.items()}
         print(f"Checking row: {row}")
         if all(subseq[key] == row[key] for key in common_keys):
