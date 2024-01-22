e5bd545d5e076f6c3c77a8e8cd8f1a8329e82004
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-22 13:06:41 +0000

    Mon, Jan 22, 2024, 9:06 PM +08:00

--- PSET_6/dna/dna.py
+++ PSET_6/dna/dna.py
@@ -35,9 +35,10 @@ def main():
     common_keys = set(dna_db[0].keys()) & set(subseq.keys())
     match = False
     for row in dna_db:
-        
+        row_values = {key: int(value) if isinstance(value, str) and value.isdigit() else value
+                      for key, value in row.items()}
         print(f"Checking row: {row}")
-        if all(subseq[key] == row[key] for key in common_keys):
+        if all(subseq[key] == row_values[key] for key in common_keys):
             if 'name' in row:
                 print(f"{row['name']}")
             match = True
