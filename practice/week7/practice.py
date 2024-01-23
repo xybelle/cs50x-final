6fae7de682bb65e2e1d18b3738c3331b515725fd
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-23 12:25:44 +0000

    Tue, Jan 23, 2024, 8:25 PM +08:00

--- practice/week7/practice.py
+++ practice/week7/practice.py
@@ -1,7 +1,6 @@
 import csv
 
 with open("something.csv", "r") as file:
-    reader = csv.reader(file)
-    next(reader)
+    reader = csv.DictReader(file)
     for row in reader:
             print(row[1])
