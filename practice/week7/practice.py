c148bd9a8f5dfa29c5229ad9734a98340e00cf52
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-23 12:28:38 +0000

    Tue, Jan 23, 2024, 8:28 PM +08:00

--- practice/week7/practice.py
+++ practice/week7/practice.py
@@ -2,5 +2,7 @@ import csv
 
 with open("something.csv", "r") as file:
     reader = csv.DictReader(file)
+    scratch, c, python = 0, 0, 0
+
     for row in reader:
-        print(row["language"])
+        favorite = 
