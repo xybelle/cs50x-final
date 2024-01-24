bc7eb7dc4f6e11f09a837a25355d1145b809a4e9
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-24 12:59:11 +0000

    Wed, Jan 24, 2024, 8:59 PM +08:00

--- PSET_7/movies/11.sql
+++ PSET_7/movies/11.sql
@@ -1,5 +1,9 @@
 SELECT title FROM movies
-WHERE id IN (
-    SELECT movie_id FROM stars
-    WHERE 
-)
+JOIN ratings ON movies.id = ratings.movie_id
+WHERE movies.id IN (
+    SELECT id FROM stars
+    WHERE person_id = (
+        SELECT id FROM people
+        WHERE name = 'Chadwick Boseman'
+    )
+) ORDER BY rating DESC LIMIT 5;
