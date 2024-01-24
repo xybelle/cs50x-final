3ecab876546175fedd55a7bb6a5259ea458c1bee
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-24 14:52:13 +0000

    Wed, Jan 24, 2024, 10:52 PM +08:00

--- PSET_7/fiftyville/log.sql
+++ PSET_7/fiftyville/log.sql
@@ -30,3 +30,8 @@ SELECT name, phone_number, passport_number
 FROM people
 WHERE license_plate IN ('R3G7486', '13FNH73', '5P2BI95', '94KL13X', '6P58WS2', '4328GD8',
     'G412CB7', 'L93JTIZ', '322W7JE', '0NTHK55', '1106N58')
+
+-- Check airports table to get origin_airport_id
+SELECT id, full_name
+FROM airports
+WHERE 
