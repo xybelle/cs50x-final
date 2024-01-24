2edb12aed7488c2684d7de08626d27b2743d713e
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-24 13:54:00 +0000

    Wed, Jan 24, 2024, 9:54 PM +08:00

--- PSET_7/movies/9.sql
+++ PSET_7/movies/9.sql
@@ -1,8 +1,11 @@
-SELECT name FROM people
+SELECT name
+FROM people
 WHERE id IN (
-    SELECT person_id FROM stars
+    SELECT person_id
+    FROM stars
     WHERE movie_id = (
-        SELECT id FROM movies
+        SELECT id
+        FROM movies
         WHERE year = '2004'
     )
 ) ORDER BY birth;
