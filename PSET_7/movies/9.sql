13217a4b2e6a637df1c2af8860724e28fb8e229a
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-24 12:21:41 +0000

    Wed, Jan 24, 2024, 8:21 PM +08:00

--- PSET_7/movies/9.sql
+++ PSET_7/movies/9.sql
@@ -1,6 +1,2 @@
-SELECT name FROM people ORDER BY birth
-JOIN stars ON people.id = stars.person_id
-WHERE stars.movie_id in (
-    SELECT id FROM movies
-    WHERE year = '2004'
-);
+SELECT id FROM movies
+WHERE year = '2004' AND 
