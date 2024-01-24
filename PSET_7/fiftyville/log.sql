b6f2b9c79047a3c399b47acb5f49325be6b39099
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-24 15:51:01 +0000

    Wed, Jan 24, 2024, 11:51 PM +08:00

--- PSET_7/fiftyville/log.sql
+++ PSET_7/fiftyville/log.sql
@@ -22,7 +22,7 @@ WHERE year = 2023 AND month = 07 AND day = 28;
 -- Ruth (withness) mentioned sometime within 10 mins of the theft, thief got into a car.
 -- Eugene (withness) saw thief withdrawing some money on an ATM on Leggett Street.
 -- Raymond (witness) saw thief called someone on the phone (< 1 minute). Asked the other person
-    -- to purchase flight ticket for earliest flight out of Fiftyville on 19th July.
+    -- to purchase flight ticket for earliest flight out of Fiftyville on 29th July.
 
 -- Check people table with license_plate(s) gathered from bakery_security_logs
 SELECT name, phone_number, passport_number
