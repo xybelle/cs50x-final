9f10bb090a0baa3b7843af27e800d80aca4cce0a
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-24 15:55:29 +0000

    Wed, Jan 24, 2024, 11:55 PM +08:00

--- PSET_7/fiftyville/log.sql
+++ PSET_7/fiftyville/log.sql
@@ -7,12 +7,7 @@
 SELECT description
 FROM crime_scene_reports
 WHERE year = 2023 AND day = 28 AND month = 07;
-
 -- Theft took place 10.15am at the bakery. There are 3 witnesses.
--- Check bakery_security_logs around 10.15am
-SELECT hour, minute, activity, license_plate
-FROM bakery_security_logs
-WHERE year = 2023 AND day = 28 AND month = 07 AND hour = 10;
 
 -- Check interviews of the 3 witnesses
 SELECT id, name, transcript
@@ -23,6 +18,7 @@ WHERE year = 2023 AND month = 07 AND day = 28;
 -- Eugene (withness) saw thief withdrawing some money on an ATM on Leggett Street.
 -- Raymond (witness) saw thief called someone on the phone (< 1 minute). Asked the other person
     -- to purchase flight ticket for earliest flight out of Fiftyville on 29th July.
+    
 
 -- Check people table with license_plate(s) gathered from bakery_security_logs
 SELECT name, phone_number, passport_number
