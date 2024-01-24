b487dde7e4394a1cb7b0782a92c7b36b1fa6c846
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-24 16:41:12 +0000

    Thu, Jan 25, 2024, 12:41 AM +08:00

--- PSET_7/fiftyville/practice.sql
+++ PSET_7/fiftyville/practice.sql
@@ -61,9 +61,9 @@ WHERE license_plate in ('1106N58', '94KL13X');
 
 -- The above didn't really help.
 
--- Check who Bruce called
+-- Check who Taylor called
 SELECT name FROM people
 WHERE phone_number IN (
     SELECT receiver FROM phone_calls
-    WHERE caller = '(367) 555-5533' AND duration <= 60
+    WHERE caller = '(286) 555-6063' AND duration <= 60
 );
