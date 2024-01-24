9882fa438d5b78cd17e015cad87fceaa046f7d11
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-24 15:00:26 +0000

    Wed, Jan 24, 2024, 11:00 PM +08:00

--- PSET_7/fiftyville/log.sql
+++ PSET_7/fiftyville/log.sql
@@ -29,7 +29,7 @@ WHERE year = 2023 AND month = 07 AND day = 28;
 SELECT name, phone_number, passport_number
 FROM people
 WHERE license_plate IN ('R3G7486', '13FNH73', '5P2BI95', '94KL13X', '6P58WS2', '4328GD8',
-    'G412CB7', 'L93JTIZ', '322W7JE', '0NTHK55', '1106N58')
+    'G412CB7', 'L93JTIZ', '322W7JE', '0NTHK55', '1106N58');
 
 -- Check passengers table to find if anyone from the people left or booked a flight out
     -- of Fiftyville
--- PSET_7/fiftyville/q.sql
+++ PSET_7/fiftyville/q.sql
@@ -1,4 +1,4 @@
 SELECT name, phone_number, passport_number
 FROM people
 WHERE license_plate IN ('R3G7486', '13FNH73', '5P2BI95', '94KL13X', '6P58WS2', '4328GD8',
-    'G412CB7', 'L93JTIZ', '322W7JE', '0NTHK55', '1106N58')
+    'G412CB7', 'L93JTIZ', '322W7JE', '0NTHK55', '1106N58');
