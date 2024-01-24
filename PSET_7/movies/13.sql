80e438ba2ff6e4c8e3277aa86119a59d8a05bfeb
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-24 13:38:31 +0000

    Wed, Jan 24, 2024, 9:38 PM +08:00

--- PSET_7/movies/13.sql
+++ PSET_7/movies/13.sql
@@ -2,7 +2,7 @@ SELECT name FROM people
 WHERE id IN (
     SELECT person_id FROM stars
     WHERE movie_id in (
-        SELECT movie_id FROM STARS
+        SELECT movie_id FROM stars
         WHERE person_id IN (
             SELECT id IN PEOPLE
             WHERE name = 'Kevin Bacon' AND birth = '1958'
