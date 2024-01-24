d40b4dbc3ad674715195d563084ccc8d52412c4e
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-24 14:12:24 +0000

    Wed, Jan 24, 2024, 10:12 PM +08:00

--- PSET_7/fiftyville/log.sql
+++ PSET_7/fiftyville/log.sql
@@ -3,6 +3,9 @@
 -- To see schema
 .schema
 
-
+-- Check crime_scene_reports for crime that took place on 28th July, 2023 on Humphrey Street
+SELECT *
+FROM crime_scene_reports
+WHERE year = '2023' AND day = '28' AND month = 'July';
 
 
