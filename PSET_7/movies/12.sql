3c4c16e288ecf9dd1bd1d89de4293327a3dd0df0
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-24 13:06:49 +0000

    Wed, Jan 24, 2024, 9:06 PM +08:00

--- PSET_7/movies/12.sql
+++ PSET_7/movies/12.sql
@@ -3,6 +3,13 @@ WHERE id in (
     SELECT movie_id FROM stars
     WHERE person_id in (
         SELECT id FROM people
-        WHERE name in ('Bradley Cooper', 'Jennifer Lawrence')
+        WHERE name = 'Bradley Cooper'
+    )
+)
+AND id in (
+    SELECT movie_id FROM stars
+    WHERE person_id in (
+        SELECT id FROM people
+        WHERE name = 'Jennifer Lawrence'
     )
 );
