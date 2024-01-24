81650907d4d7a3b98cda28afe5ec5c27590ff9e6
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-24 16:42:20 +0000

    Thu, Jan 25, 2024, 12:42 AM +08:00

--- PSET_7/fiftyville/log.sql
+++ PSET_7/fiftyville/log.sql
@@ -7,9 +7,10 @@
 SELECT description
 FROM crime_scene_reports
 WHERE year = 2023 AND day = 28 AND month = 07;
+
 -- Theft took place 10.15am at the bakery. There are 3 witnesses.
 
--- Check interviews of the 3 witnesses
+-- Check interviews of the 3 witnesses if there are more info
 SELECT id, name, transcript
 FROM interviews
 WHERE year = 2023 AND month = 07 AND day = 28;
@@ -19,52 +20,41 @@ WHERE year = 2023 AND month = 07 AND day = 28;
 -- Raymond (witness) saw thief called someone on the phone (< 1 minute). Asked the other person
     -- to purchase flight ticket for earliest flight out of Fiftyville on 29th July.
 
--- Check bakery_security_logs around 10.15am
-SELECT hour, minute, activity, license_plate
-FROM bakery_security_logs
-WHERE year = 2023 AND day = 28 AND month = 07 AND hour = 10;
-
--- Check people table with license_plate(s) gathered from bakery_security_logs
-SELECT name, phone_number, passport_number
-FROM people
-WHERE license_plate IN ('R3G7486', '13FNH73', '5P2BI95', '94KL13X', '6P58WS2', '4328GD8',
-    'G412CB7', 'L93JTIZ', '322W7JE', '0NTHK55', '1106N58');
-
 -- Get origin_airport_id of Fiftyville
 SELECT * FROM airports
+
 -- | 8 | CSF | Fiftyville Regional Aiport | Fiftyville |
 
--- Get flight_id out of Fiftyville on July 29, 2023 and its destination
-SELECT id, destination_airport_id
-FROM flights
+-- Check earliest flight out of Fiftyville
+SELECT * FROM flights
 WHERE origin_airport_id = 8 AND year = 2023 AND month = 07 AND day = 29;
 
--- Check passengers table to find if anyone from the people left or booked a flight out
-    -- of Fiftyville
-SELECT *
-FROM passengers
-WHERE passport_number in (2963008352, 7526138472, 7874488539, 7049073643, 1695452385,
-    1988161715, 8496433585, 3592750733, 8294398571, 5773159633, 3642612721)
-    AND flight_id in (18, 23, 36, 43, 53);
+-- Earliest flight out of Fiftyville is flight_id: 36 followed by 43, 23, 53, 18
 
--- Check atm_tansactions
-SELECT name
-FROM people
-WHERE id IN (
-    SELECT person_id
-    FROM bank_accounts
-    WHERE account_number IN (
-        SELECT account_number
-        FROM atm_transactions
-        WHERE year = 2023 AND month = 07 AND day = 28 AND transaction_type = 'withdraw'
-            AND atm_location = 'Leggett Street'
+-- Check passengers who booked flight_id: 36, who may have also withdrew money from ATM on
+    -- Leggett Street on the 28th, and made a phone call to someone with less than a minute
+    -- duration, who may also be around the bakery around 10.
+SELECT name, phone_number, license_plate FROM people
+WHERE passport_number in (
+    SELECT passport_number FROM passengers
+    WHERE flight_id = 36
+) AND id IN (
+    SELECT person_id FROM bank_accounts
+    WHERE account_number in (
+        SELECT account_number FROM atm_transactions
+        WHERE atm_location = 'Leggett Street' AND transaction_type = 'withdraw' AND year = 2023
+            AND month = 07 AND day = 28
     )
+) AND phone_number in (
+    SELECT caller FROM phone_calls
+    WHERE duration <= 60 AND year = 2023 AND month = 07 AND day = 28
+) AND license_plate in (
+    SELECT license_plate FROM bakery_security_logs
+    WHERE year = 2023 AND month = 07 AND day = 28 AND hour = 10
 );
 
--- Comparing the names of people that flew out of Fiftyville on 29th July and people who
-    -- withdrew money on the ATM on Leggett Street, narrowed names to Diana and Bruce.
+-- Narrowed down to Taylor and Bruce
 
--- Check phonecalls
-SELECT caller, receiver
-FROM phone_calls
-WHERE duration < 60 AND year = 2023 AND month = 07 AND day = 28;
+-- Check bakery security logs around what time both
+SELECT license_plate, activity, hour, minute FROM bakery_security_logs
+WHERE license_plate in ('1106N58', '94KL13X');
