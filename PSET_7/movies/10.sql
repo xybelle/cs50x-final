f496cdd8f0ffe1f04e136d71ba1fcbf63c9bc145
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-24 13:52:43 +0000

    Wed, Jan 24, 2024, 9:52 PM +08:00

--- PSET_7/movies/10.sql
+++ PSET_7/movies/10.sql
@@ -1,8 +1,10 @@
-SELECT name FROM people
+SELECT name
+FROM people
 WHERE id IN (
-    SELECT person_id FROM directors
-    WHERE movie_id = (
-        SELECT id FROM ratings
+    SELECT person_id
+    FROM directors
+    WHERE movie_id in (
+        SELECT movie_id FROM ratings
         WHERE rating >= 9.0
     )
 );
