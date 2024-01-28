d3c4813940907ac013eb948d726e9111efd0bf4b
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-28 16:24:46 +0000

    Mon, Jan 29, 2024, 12:24 AM +08:00

--- practice/week8/registration/app.py
+++ practice/week8/registration/app.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-CLASS = ["Ballet", "Jazz", "Hip-Hop"]
+CLASSES = ["Ballet", "Jazz", "Hip-Hop"]
 
 @app.route("/", methods=["POST"])
-    return render_template("index.html", class=CLASS)
+    return render_template("index.html", class=CLASSES)
 
 @app.route("/register", methods=["POST"])
 def register():
     if not request.form.get("fname") or not request.form.get("sname")
-            or not request.form.get("email"):
+            or not request.form.get("email") or not in CLASSES:
         return render_template("fail.html")
     return render_template("success.html")
