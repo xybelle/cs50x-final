e7a7a0ec01f7ec7fbea80cbada836a0d65948bbe
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-28 16:11:15 +0000

    Mon, Jan 29, 2024, 12:11 AM +08:00

--- practice/week8/registration/app.py
+++ practice/week8/registration/app.py
@@ -3,13 +3,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
-    if request.method == "POST":
-        name = request.form.get("name", "world")
-        return render_template("register.html", name=name)
-    else:
-        return render_template("index.html")
+    return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=["POST"])
 def register():
     if not request.form.get("name"):
         return "failure"
