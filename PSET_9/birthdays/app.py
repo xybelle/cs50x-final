85044918c8926126a462b2df90fdb288ca979377
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-29 13:10:56 +0000

    Mon, Jan 29, 2024, 9:10 PM +08:00

--- PSET_9/birthdays/app.py
+++ PSET_9/birthdays/app.py
@@ -31,7 +31,7 @@ def index():
         month = request.form.get("month")
         day = request.form.get("day")
         if not name or not month or not day:
-            redirect("/")
+            return redirect("/", message:)
 
         db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", name, month, day)
         return redirect("/")
