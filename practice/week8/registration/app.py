7f6ed026a11ca991caea977f10dc8f304734622b
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-29 08:19:00 +0000

    Mon, Jan 29, 2024, 4:19 PM +08:00

--- practice/week8/registration/app.py
+++ practice/week8/registration/app.py
@@ -1,9 +1,13 @@
 from cs50 import SQL
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
 
 db = SQL("sqlite:///gds.db")
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 CLASSES = ["Ballet", "Jazz", "Hip-Hop"]
 
