f650164487d11ee842f78efb945dfb4f9bbd48d5
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-29 10:37:21 +0000

    Mon, Jan 29, 2024, 6:37 PM +08:00

--- PSET_9/birthdays/app.py
+++ PSET_9/birthdays/app.py
@@ -40,7 +40,7 @@ def index():
 
         # TODO: Display the entries in the database on index.html
 
-        birthdays = db.execute("SELECT * FROM birthdays")
-        return render_template("index.html", birthdays=birthdays)
+        bdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", birthdays=bdays)
 
 
