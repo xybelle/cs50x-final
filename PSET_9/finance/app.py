81442cf8ff42992d14fcb74c8a7d0b9736d7a3a2
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-30 09:26:29 +0000

    Tue, Jan 30, 2024, 5:26 PM +08:00

--- PSET_9/finance/app.py
+++ PSET_9/finance/app.py
@@ -38,11 +38,12 @@ def index():
     stocks = db.execute("SELECT DISTINCT stock FROM stocks WHERE user_id = ?", session["user_id"])
     shares_owned = db.execute(
         "SELECT SUM(shares) FROM stocks WHERE user_id = ? GROUP BY stock", session["user_id"])
+    price
 
     for stock in stocks:
         price[] = lookup(stocks(stock))
 
-    
+
     return apology("TODO")
 
 
