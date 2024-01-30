1650caacb3b1f092a2b54f603b2e42f243dea4a8
xybelle <150109343+xybelle@users.noreply.github.com>
2024-01-30 07:43:48 +0000

    Tue, Jan 30, 2024, 3:43 PM +08:00

--- PSET_9/finance/app.py
+++ PSET_9/finance/app.py
@@ -35,7 +35,7 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    stocks = db.execute("SELECT stock FROM stocks WHERE user_id = ?), session["user_id"]
+    stocks = db.execute("SELECT stock FROM stocks WHERE user_id = ?", session["user_id"])
     shares_owned =
     return apology("TODO")
 
