68ba155894714bfe2cf35fdd1f80c7c2de2fac1d
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-06 10:09:14 +0000

    Tue, Feb 6, 2024, 6:09 PM +08:00

--- cs50p/pset1/extensions/extensions.py
+++ cs50p/pset1/extensions/extensions.py
@@ -7,6 +7,6 @@ if f[2] == 'gif' or f[2] == 'jpg' or f[2] == 'jpeg' or f[2] == 'png':
 elif f[2] == 'pdf' or f[2] == 'zip':
     print(f"application/{f[2]}")
 elif f[2] == 'txt':
-    print(f"txt/{f[2]}")
+    print(f"text/{f[2]}")
 else:
     print("application/octet-stream")
