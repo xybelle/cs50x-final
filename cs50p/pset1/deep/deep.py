a571f24727a4526984c8d2a3ce3f0662977f4988
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-06 09:23:28 +0000

    Tue, Feb 6, 2024, 5:23 PM +08:00

--- cs50p/pset1/deep/deep.py
+++ cs50p/pset1/deep/deep.py
@@ -1,4 +1,4 @@
-q = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").casefold()
+q = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").casefold().lstrip().remove
 if q == "42" or q == "forty two" or q == "forty-two":
     print("Yes")
 else:
