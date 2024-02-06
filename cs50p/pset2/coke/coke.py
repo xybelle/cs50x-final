0fca96abb570ade77baff8357457ad44ca621820
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-06 16:03:34 +0000

    Wed, Feb 7, 2024, 12:03 AM +08:00

--- cs50p/pset2/coke/coke.py
+++ cs50p/pset2/coke/coke.py
@@ -2,7 +2,7 @@ amount_due, c = 50, 0
 valid_c = [25, 10, 5]
 
 while True:
-    print(f"Amount due: {amount_due}\n")
+    print(f"Amount due: {amount_due}")
     c = int(input("Insert coin: "))
     if c not in valid_c:
         continue
