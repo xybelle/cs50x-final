c645941492decb0e36c37bb95b0d9a32c54f0b7f
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-06 15:26:56 +0000

    Tue, Feb 6, 2024, 11:26 PM +08:00

--- cs50p/pset2/coke/coke.py
+++ cs50p/pset2/coke/coke.py
@@ -4,8 +4,8 @@ valid_c = [25, 10, 5]
 while c <= 50:
     print(f"Amount due: {amount_due}")
     c = int(input("Insert coin: "))
-    c += c
     if c in valid_c:
+        c += c
         amount_due -= c
         if c <= 4:
             print(f"Change owed: {change}", change = c - amount_due)
