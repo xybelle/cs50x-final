ec754048867086d41481bf4325660d8c1ab382cd
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-06 10:01:59 +0000

    Tue, Feb 6, 2024, 6:01 PM +08:00

--- cs50p/pset1/extensions/extensions.py
+++ cs50p/pset1/extensions/extensions.py
@@ -1,9 +1,9 @@
-f = input("File name: ").lower()
-i = "image/"
-a = "application/"
+f = input("File name: ").lower().partition('.')
 
 if f.endswith('gif') or f.endswith('jpg') or f.endswith('jpeg') or f.endswith('png'):
-    i = f.partition('.')
-    print(i)
-    print(f"image/{i[2]}")
+    print(f"image/{f[2]}")
+elif f.endswith('pdf') or f.endswith('zip'):
+    print(f"application/{f[2]}")
+elif f.endswith('txt'):
+    print(f"txt/{f[2]}")
 
