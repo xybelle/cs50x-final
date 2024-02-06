d21e0349492a74f75e0c258bb627c6c836c67379
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-06 09:55:02 +0000

    Tue, Feb 6, 2024, 5:55 PM +08:00

--- cs50p/pset1/extensions/extensions.py
+++ cs50p/pset1/extensions/extensions.py
@@ -2,7 +2,7 @@ f = input("File name: ").lower()
 i = "image/"
 a = "application/"
 
-if f.endswith('gif', 'jpg', 'jpeg', 'png'):
+if f.endswith('gif') or f.endswith('jpg') or f.endswith('jpeg') or f.endswith('png'):
     i = f.index('.')
     print(f"image/{i}")
 
