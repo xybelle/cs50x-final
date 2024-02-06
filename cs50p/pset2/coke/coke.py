4489e63d7f5bd52d39f5286f75171dcb046fdeb7
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-06 15:52:16 +0000

    Tue, Feb 6, 2024, 11:52 PM +08:00

--- cs50p/pset2/coke/coke.py
+++ cs50p/pset2/coke/coke.py
@@ -5,6 +5,7 @@ while c <= 50:
     print(f"Amount due: {amount_due}")
     c = int(input("Insert coin: "))
     if c not in valid_c:
+        print("Can only accept coins in these denominations: 25 cents, 10 cents, and 5 cents")
         continue
     else:
         amount_due -= c
