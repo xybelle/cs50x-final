abd08fd92b00ab3a516296b5f269d692f601bf29
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-06 10:38:04 +0000

    Tue, Feb 6, 2024, 6:38 PM +08:00

--- cs50p/pset1/extensions/extensions.py
+++ cs50p/pset1/extensions/extensions.py
@@ -1,16 +1,17 @@
-f = input("File name: ").lower().lstrip().rstrip().split('.')
+f = input("File name: ").lower().lstrip().rstrip()
 
 if f.count('.') >= 2 or f.count('.') == 0:
     print("application/octet-stream")
 
-
-if f[2] == 'gif' or f[2] == 'png':
-    print(f"image/{f[2]}")
-elif f[2] == 'jpg' or f[2] == 'jpeg':
+f = f.split('.')
+print(f)
+if f[len(f)] == 'gif' or f[len(f)] == 'png':
+    print(f"image/{f[len(f)]}")
+elif f[len(f)] == 'jpg' or f[len(f)] == 'jpeg':
     print("image/jpeg")
-elif f[2] == 'pdf' or f[2] == 'zip':
-    print(f"application/{f[2]}")
-elif f[2] == 'txt':
+elif f[len(f)] == 'pdf' or f[len(f)] == 'zip':
+    print(f"application/{f[len(f)]}")
+elif f[len(f)] == 'txt':
     print("text/plain")
 else:
     print("application/octet-stream")
