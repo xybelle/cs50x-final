405df6c8cef699d1c885aac21eeeaaeec054c2fc
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-06 10:22:34 +0000

    Tue, Feb 6, 2024, 6:22 PM +08:00

--- cs50p/pset1/extensions/extensions.py
+++ cs50p/pset1/extensions/extensions.py
@@ -1,7 +1,6 @@
 f = input("File name: ").lower().lstrip().rstrip()
-c = f.count('.')
 
-if c >= 2:
+if f.count('.') >= 2:
     print("application/octet-stream")
 
 if f[2] == 'gif' or f[2] == 'jpg' or f[2] == 'jpeg' or f[2] == 'png':
