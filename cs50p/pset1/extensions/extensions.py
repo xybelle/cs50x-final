80ae21aa43e3a988328fb85e63c54cff28be3745
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-06 09:58:08 +0000

    Tue, Feb 6, 2024, 5:58 PM +08:00

--- cs50p/pset1/extensions/extensions.py
+++ cs50p/pset1/extensions/extensions.py
@@ -3,6 +3,7 @@ i = "image/"
 a = "application/"
 
 if f.endswith('gif') or f.endswith('jpg') or f.endswith('jpeg') or f.endswith('png'):
-    i = f.index('.')
-    print(f"image/{i}")
+    i = f.partition('.')
+    print(i)
+    print(f"image/{i[2]}")
 
