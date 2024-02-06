69a4f407227d2463c2403076a007ec9d59f6a7e6
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-06 15:04:54 +0000

    Tue, Feb 6, 2024, 11:04 PM +08:00

--- cs50p/pset2/coke/coke.py
+++ cs50p/pset2/coke/coke.py
@@ -5,7 +5,8 @@ c = int(input("Insert coin: "))
 while amount_due != 0:
     if c in valid_c:
         amount_due -= c
-        if amount_due ==
+        if amount_due == 0:
+            print("Change owed: 0")
         print(f"Amount due: {amount_due}")
         c = int(input("Insert coin: "))
     else:
