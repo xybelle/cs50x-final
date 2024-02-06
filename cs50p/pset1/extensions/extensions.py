a8692b0881f71622d7dc3c8bfef739c5ab18bba9
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-06 10:26:39 +0000

    Tue, Feb 6, 2024, 6:26 PM +08:00

--- cs50p/pset1/extensions/extensions.py
+++ cs50p/pset1/extensions/extensions.py
@@ -5,12 +5,12 @@ if f.count('.') >= 2:
 
 f = f.partition()
 
-elif f[2] == 'gif' or f[2] == 'jpg' or f[2] == 'jpeg' or f[2] == 'png':
+if f[2] == 'gif' or f[2] == 'jpg' or f[2] == 'jpeg' or f[2] == 'png':
     if f[2] == 'jpg' or f[2] == 'jpeg':
         print("image/jpeg")
     print(f"image/{f[2]}")
-elif f[2] == 'pdf' or f[2] == 'zip':
+if f[2] == 'pdf' or f[2] == 'zip':
     print(f"application/{f[2]}")
-elif f[2] == 'txt':
+if f[2] == 'txt':
     print("text/plain")
 
