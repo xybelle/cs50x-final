e5be6467a9783cefec98ca2e32eaa21864a7cfbc
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-06 10:08:35 +0000

    Tue, Feb 6, 2024, 6:08 PM +08:00

--- cs50p/pset1/extensions/extensions.py
+++ cs50p/pset1/extensions/extensions.py
@@ -1,6 +1,8 @@
 f = input("File name: ").lower().lstrip().rstrip().partition('.')
 
 if f[2] == 'gif' or f[2] == 'jpg' or f[2] == 'jpeg' or f[2] == 'png':
+    if f[2] == 'jpg' or f[2] == 'jpeg':
+        print("image/jpeg")
     print(f"image/{f[2]}")
 elif f[2] == 'pdf' or f[2] == 'zip':
     print(f"application/{f[2]}")
