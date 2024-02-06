23aece28ef11b01e33ed28700053de49251dd9b5
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-06 14:22:46 +0000

    Tue, Feb 6, 2024, 10:22 PM +08:00

---
+++ cs50p/pset1/meal/meal.py
@@ -0,0 +1,18 @@
+def main():
+    time = input("What time is it? ")
+    t = convert(time)
+
+    if t >= 7.0 and t <= 8.0:
+        print("breakfast time")
+    elif t >= 12.0 and t <= 13.0:
+        print("lunch time")
+    elif t >= 18.0 and t <= 19.0:
+        print("dinner time")
+
+
+def convert(time):
+    h, m = time.split(':')
+
+
+if __name__ == "__main__":
+    main()
