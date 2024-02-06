b65725f870bb57491292e9f41ce7cbe39f2d0f39
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-06 09:01:26 +0000

    Tue, Feb 6, 2024, 5:01 PM +08:00

--- cs50p/pset1/tip/tip.py
+++ cs50p/pset1/tip/tip.py
@@ -1,17 +1,19 @@
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
-    tip = dollars * percent
+    #tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
 
 def dollars_to_float(d):
     d = float(d.lstrip('$'))
+    print(d)
     return d
 
 
 def percent_to_float(p):
     p = float(p.removesuffix('%'))
+    print(p)
     return p
 
 
