b6f5dff8274c8ca74653013e7a6700d05ae65cce
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-08 11:20:03 +0000

    Thu, Feb 8, 2024, 7:20 PM +08:00

--- cs50p/pset3/fuel/fuel.py
+++ cs50p/pset3/fuel/fuel.py
@@ -2,6 +2,7 @@ while True:
     try:
         fraction = input("Fraction: ").split()
         percent = float(fraction[0]) / float(fraction[2])
+        break
     except ValueError:
         print("That was not a fraction")
     else:
