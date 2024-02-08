7d2e8ef3c4c03b63ce904339de657a93b312eaf2
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-08 11:29:04 +0000

    Thu, Feb 8, 2024, 7:29 PM +08:00

--- cs50p/pset3/fuel/fuel.py
+++ cs50p/pset3/fuel/fuel.py
@@ -1,9 +1,8 @@
 while True:
-    try:
+
         fraction = input("Fraction: ").split()
         percent = float(fraction[0]) / float(fraction[2])
         break
-    except ValueError:
-        pass
+
 
 print(f"{float(percent)}%")
