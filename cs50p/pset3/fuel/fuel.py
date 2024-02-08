0132f0ac14f36cea2579cefe31e087029fc0a663
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-08 11:45:22 +0000

    Thu, Feb 8, 2024, 7:45 PM +08:00

--- cs50p/pset3/fuel/fuel.py
+++ cs50p/pset3/fuel/fuel.py
@@ -2,13 +2,15 @@ while True:
     try:
         fraction = input("Fraction: ").split('/')
         percent = (int(fraction[0]) / int(fraction[1])) * 100
+
+        if percent <= 0.01:
+            print("E")
+        elif percent >= 99:
+            print("F")
+        else:
+            print(f"{int(percent)}%")
         break
-    except (ValueError, ZeroDivisionError):
+    except (ValueError, ZeroDivisionError, percent > 100):
         pass
 
-if percent <= 0.01:
-    print("E")
-elif percent >= 99:
-    print("F")
-else:
-    print(f"{int(percent)}%")
+
