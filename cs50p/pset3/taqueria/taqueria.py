d3c47e0008d645de9c8f205a0859c4d81ce8d89c
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-09 11:20:33 +0000

    Fri, Feb 9, 2024, 7:20 PM +08:00

--- cs50p/pset3/taqueria/taqueria.py
+++ cs50p/pset3/taqueria/taqueria.py
@@ -18,7 +18,7 @@ while True:
         if item not in menu:
             raise KeyError
         total += menu[f'{item}']
-        print(f"${float(total)}")
+        print(f"${float(total):.2f}")
     except KeyError:
         pass
     except EOFError:
