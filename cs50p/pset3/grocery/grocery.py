9e762ffb39cd23479491f24f7af26bcfdb15db85
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-09 11:38:17 +0000

    Fri, Feb 9, 2024, 7:38 PM +08:00

--- cs50p/pset3/grocery/grocery.py
+++ cs50p/pset3/grocery/grocery.py
@@ -1,8 +1,8 @@
-grocery_list = {'item'}
+grocery_list = {}
 while True:
     try:
         item = input()
-        grocery_list['item'] = item
+        grocery_list['ls'] = item
 
     except EOFError:
         print(grocery_list)
