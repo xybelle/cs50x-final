5b14c138c2be5922f4d015dd2d983fa3849d99d1
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-09 11:14:07 +0000

    Fri, Feb 9, 2024, 7:14 PM +08:00

--- cs50p/pset3/taqueria/taqueria.py
+++ cs50p/pset3/taqueria/taqueria.py
@@ -18,7 +18,7 @@ while True:
         if item not in menu:
             raise KeyError
         total += menu[f'{item}']
-        print(f"${round(total, 3)}")
+        print(f"${float(total).format(#)}")
     except KeyError:
         pass
     except EOFError:
