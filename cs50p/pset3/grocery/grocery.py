cb9164d1b101c8072515d4bb6db5bfe662dd5b47
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-09 21:35:48 +0000

    Sat, Feb 10, 2024, 5:35 AM +08:00

--- cs50p/pset3/grocery/grocery.py
+++ cs50p/pset3/grocery/grocery.py
@@ -3,7 +3,7 @@ while True:
     try:
         item = input()
         #ls = {'name': item, 'amt': 1}
-        grocery_list = item
+        grocery_list[item] = 1
     except EOFError:
         for item in grocery_list:
             print(f"{grocery_list.get(item)} {item.upper()}")
