cb2d782fe72e6f8be25f18ecd0f01b999127d526
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-09 23:09:06 +0000

    Sat, Feb 10, 2024, 7:09 AM +08:00

--- cs50p/pset3/outdated/outdated.py
+++ cs50p/pset3/outdated/outdated.py
@@ -52,7 +52,7 @@ def check_year(year):
 
 
 def validate_date(mdy):
-    if mdy.isalnum() and ' ' in mdy and ',' in mdy:
+    if mdy.isalnum() and ',' in mdy:
         return mdy.split()
     elif '/' in mdy and not mdy.isalnum():
         return mdy.split('/')
