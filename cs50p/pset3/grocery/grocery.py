27a31df98a1a59bc50d5654e45592cdb876e81ee
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-09 11:48:30 +0000

    Fri, Feb 9, 2024, 7:48 PM +08:00

--- cs50p/pset3/grocery/grocery.py
+++ cs50p/pset3/grocery/grocery.py
@@ -3,7 +3,7 @@ while True:
     try:
         item = input()
         grocery_list[item] = 1
-
+        grocery_list.get(item)
     except EOFError:
-        print(grocery_list.get())
+        print(grocery_list)
         break
