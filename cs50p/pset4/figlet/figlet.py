07f8a93a405b16844d3fd2abbf3eab7f3e5b32f0
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-12 16:19:01 +0000

    Tue, Feb 13, 2024, 12:19 AM +08:00

--- cs50p/pset4/figlet/figlet.py
+++ cs50p/pset4/figlet/figlet.py
@@ -9,7 +9,13 @@ fonts = figlet.getFonts()
 
 if len(sys.argv) > 2:
     sys.exit("Too many arguments")
-if not sys.argv[1] == '--font' or not sys.argv[1] == '-f':
+elif not sys.argv[1] == '--font' or not sys.argv[1] == '-f':
     sys.exit("Invalid argument")
 elif sys.argv[2] not in fonts:
     sys.exit("Invalid font name")
+
+figlet.setFont(font=sys.argv[2])
+
+msg = input("Input: ")
+
+print(figlet.renderText(msg))
