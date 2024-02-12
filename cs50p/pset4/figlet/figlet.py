786bed6a07fb83c3510135903afdbddde0f6d749
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-12 16:12:23 +0000

    Tue, Feb 13, 2024, 12:12 AM +08:00

--- cs50p/pset4/figlet/figlet.py
+++ cs50p/pset4/figlet/figlet.py
@@ -1,6 +1,13 @@
+import sys
+
 from pyfiglet import Figlet
 
 figlet = Figlet()
 
-figlet.getFonts()
+a = figlet.getFonts()
 
+print(a)
+
+if len(sys.argv) > 2:
+    sys.exit("Too many arguments")
+if
