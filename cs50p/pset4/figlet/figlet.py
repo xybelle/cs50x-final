b261cbf6a8794a87a6043cb8821e5dad034d6a8e
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-12 16:34:29 +0000

    Tue, Feb 13, 2024, 12:34 AM +08:00

--- cs50p/pset4/figlet/figlet.py
+++ cs50p/pset4/figlet/figlet.py
@@ -8,20 +8,21 @@ figlet = Figlet()
 
 fonts = figlet.getFonts()
 
-if len(sys.argv) > 3:
-    sys.exit("Too many arguments")
-elif sys.argv[1] != '--font' and sys.argv[1] != '-f':
-    sys.exit("Invalid argument")
-elif sys.argv[2] not in fonts:
-    sys.exit("Invalid font name")
-elif len(sys.argv) == 0:
+def main():
+    if len(sys.argv) > 3:
+        sys.exit("Too many arguments")
+    elif sys.argv[1] != '--font' and sys.argv[1] != '-f':
+        sys.exit("Invalid argument")
+    elif sys.argv[2] not in fonts:
+        sys.exit("Invalid font name")
+    elif len(sys.argv) == 0:
 
 
-figlet.setFont(font=sys.argv[2])
+    figlet.setFont(font=sys.argv[2])
 
-msg = input("Input: ")
+    msg = input("Input: ")
 
-print(figlet.renderText(msg))
+    print(figlet.renderText(msg))
 
 
 def random_font(msg):
@@ -30,5 +31,6 @@ def random_font(msg):
     print(figlet.renderText(msg))
 
 
-def specific_font(msg):
-    
+def specific_font(msg, font):
+    figlet.setfont(font=font)
+    print(figlet.renderText(msg))
