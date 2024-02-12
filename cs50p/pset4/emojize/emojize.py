9ec561fea22bae719c1b23d136093c4ec34f828e
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-12 15:24:28 +0000

    Mon, Feb 12, 2024, 11:24 PM +08:00

--- cs50p/pset4/emojize/emojize.py
+++ cs50p/pset4/emojize/emojize.py
@@ -3,10 +3,13 @@ import emoji
 a = input("Input: ")
 print(a)
 
+word = {}
+i = 0
 for c in a:
     if c = ' ':
         continue
-    
+    word[0] = c
+
 for _ in a:
     if ':' in _:
         print(emoji.emojize('_'))
