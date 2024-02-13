3429670d91ef220b1d6d1283d0b3a90a96434173
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-13 16:53:19 +0000

    Wed, Feb 14, 2024, 12:53 AM +08:00

--- cs50p/pset4/bitcoin/bitcoin.py
+++ cs50p/pset4/bitcoin/bitcoin.py
@@ -5,16 +5,21 @@ import sys
 
 if len(sys.argv) != 2:
     sys.exit("Usage: python bitcoin.py n")
-elif not sys.argv[2].isdigit():
+elif not sys.argv[1].isdigit():
     sys.exit("n is not an integer")
 
-n = float(sys.argv[2])
+n = float(sys.argv[1])
 
 try:
     response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
 except requests.RequestException:
     sys.exit()
 
-res = response.json()
-price = bpi USD rate
-current_cost = n *
+p = {"bpi":{"USD":{"rate"}}}
+r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json", params=p)
+#res = response.json()
+#price = bpi USD rate
+#current_cost = n *
+
+print(p)
+print(r)
