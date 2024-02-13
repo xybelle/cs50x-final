e83584f736b56291389b4d315f9dc452f36fc7fe
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-13 00:39:34 +0000

    Tue, Feb 13, 2024, 8:39 AM +08:00

--- cs50p/pset4/game/game.py
+++ cs50p/pset4/game/game.py
@@ -1,14 +1,32 @@
 import random
 
-while True:
-    try:
-        n = input("Level: ")
-        if n <= 0:
-            raise ValueError
-        guess(n)
-    except ValueError:
-        pass
+
+def main():
+    while True:
+        try:
+            number = input("Level: ")
+            if n <= 0:
+                raise ValueError
+            guess(n)
+        except ValueError:
+            pass
 
 
-def guess(n):
-    
+def guess(number):
+    while True:
+        try:
+            n = random.randint(1, number)
+            g = input("Guess: ")
+            if g == n:
+                print("Just right!")
+            elif g < n:
+                print("Too small!")
+            elif g > n:
+                print("Too large!")
+            else:
+                raise ValueError
+        except ValueError:
+            pass
+
+
+main()
