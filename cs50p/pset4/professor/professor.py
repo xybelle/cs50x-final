0fb907fa7ffbb7942454c1b3d8c0f663b1c000dc
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-13 01:46:34 +0000

    Tue, Feb 13, 2024, 9:46 AM +08:00

--- cs50p/pset4/professor/professor.py
+++ cs50p/pset4/professor/professor.py
@@ -5,14 +5,15 @@ def main():
     n = get_level()
     correct_ans = 0
     for i in range(10):
-        x = generate_integer(n)
-        y = generate_integer(n)
-        ans = input(f"{x} + {y} = ")
-        if x + y == ans:
-            correct_ans += 1
-            continue
-        else:
-            
+        try:
+            x = generate_integer(n)
+            y = generate_integer(n)
+            ans = input(f"{x} + {y} = ")
+            if x + y == ans:
+                correct_ans += 1
+                continue
+            else:
+
 
 def get_level():
     while True:
