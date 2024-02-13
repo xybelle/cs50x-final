deca847b6e7eaadb600f43f96c6386357bf88612
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-13 16:25:10 +0000

    Wed, Feb 14, 2024, 12:25 AM +08:00

--- cs50p/pset4/bitcoin/bitcoin.py
+++ cs50p/pset4/bitcoin/bitcoin.py
@@ -8,3 +8,8 @@ elif not sys.argv[2].isdigit():
     sys.exit("n is not an integer")
 
 n = float(sys.argv[2])
+
+try:
+    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+except requests.RequestException:
+    sys.exit()
