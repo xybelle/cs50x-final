747d3184415aeaf270bae53b0c4a1224f3a31445
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-13 01:38:27 +0000

    Tue, Feb 13, 2024, 9:38 AM +08:00

--- cs50p/pset4/professor/professor.py
+++ cs50p/pset4/professor/professor.py
@@ -10,13 +10,18 @@ def get_level():
             level = int(input("Level: "))
             if level > 3 or level <= 0:
                 raise ValueError
-            break
+            generate_integer(level)
         except ValueError:
             pass
 
 
 def generate_integer(level):
-    ...
+    if level == 1:
+        random_number = random.randint(1, 9)
+    elif level == 2:
+        random_number = random.randint(10, 99)
+    elif level == 3:
+        random_number = random.randint(100, 999)
 
 
 if __name__ == "__main__":
