5a49f26f4b21e50a27c7d4ce17ec4a69a4ed52fd
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-13 00:43:47 +0000

    Tue, Feb 13, 2024, 8:43 AM +08:00

--- cs50p/pset4/game/game.py
+++ cs50p/pset4/game/game.py
@@ -8,6 +8,7 @@ def main():
             if number <= 0:
                 raise ValueError
             guess(number)
+            break
         except ValueError:
             pass
 
@@ -19,10 +20,13 @@ def guess(number):
             g = int(input("Guess: "))
             if g == n:
                 print("Just right!")
+                break
             elif g < n:
                 print("Too small!")
+                break
             elif g > n:
                 print("Too large!")
+                break
             else:
                 raise ValueError
         except ValueError:
