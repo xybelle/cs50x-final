761507ac7093d4d75643ed8b2e97ee436a3b39a9
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-13 01:15:54 +0000

    Tue, Feb 13, 2024, 9:15 AM +08:00

--- cs50p/pset4/game/game.py
+++ cs50p/pset4/game/game.py
@@ -18,7 +18,7 @@ def guess_number(n):
     while True:
         try:
             g = int(input("Guess: "))
-            if g < 0:
+            if g < 0 and g > n:
                 raise ValueError
             if g == n:
                 print("Just right!")
