a6dd7999a9e61f7c7b5332cd8b8244d38b283437
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-14 00:13:33 +0000

    Wed, Feb 14, 2024, 8:13 AM +08:00

--- cs50p/pset5/test_twttr/test_twttr.py
+++ cs50p/pset5/test_twttr/test_twttr.py
@@ -1,5 +1,13 @@
 from twttr import shorten
 
 
-def test_shorten():
+def test_lowercase():
     assert shorten("how are you") == "hw r y"
+
+
+def test_uppercase():
+    assert shorten("HOW ARE YOU") == "HW R Y"
+
+
+def test_numbers():
+    assert shorten("1 day Two DAys 3 days")
