d8a1b2600fc9a1e093262e29d59837361e446226
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-14 01:57:14 +0000

    Wed, Feb 14, 2024, 9:57 AM +08:00

--- cs50p/pset5/test_fuel/test_fuel.py
+++ cs50p/pset5/test_fuel/test_fuel.py
@@ -4,8 +4,17 @@ from fuel import convert, gauge
 def test_convert():
     assert convert("1/4") == 25
     assert convert("3/4") == 75
-    assert convert("4/3") == ValueError
-    assert convert("2/0") == ZeroDivisionError
+
+
+def test_convert_ValueError():
+    with pytest.raises(ValueError):
+        convert("4/3")
+
+
+def test_convert_ZeroDivisionError():
+    with pytest.raises(ZeroDivisionError):
+        convert("2/0")
+
 
 def test_gauge():
     assert gauge(1) == "E"
