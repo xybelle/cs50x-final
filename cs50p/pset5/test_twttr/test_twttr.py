23b87610777ef3041fc3bd2e26e9b0ba52d2599e
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-14 00:15:37 +0000

    Wed, Feb 14, 2024, 8:15 AM +08:00

--- cs50p/pset5/test_twttr/test_twttr.py
+++ cs50p/pset5/test_twttr/test_twttr.py
@@ -10,4 +10,8 @@ def test_uppercase():
 
 
 def test_numbers():
-    assert shorten("1 day Two DAys 3 days")
+    assert shorten("1 day Two DAys 3 days") == "1 dy Tw Dys 3 dys"
+
+
+def test_punc():
+    assert shorten("Can you hear me?") == "Cn y hr m?"
