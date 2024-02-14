50369910e9f5979391c7d0e94fd658aec5205678
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-14 01:07:21 +0000

    Wed, Feb 14, 2024, 9:07 AM +08:00

--- cs50p/pset5/test_bank/test_bank.py
+++ cs50p/pset5/test_bank/test_bank.py
@@ -3,15 +3,17 @@ from bank import value
 
 def test_hello():
     assert value("hello") == 0
+    assert value("HELLO") == 0
 
 
 def test_h():
     assert value("how do you do") == 20
+    assert value("HOW DO YOU DO") == 20
 
 
 def test_others():
-    assert value("what's up?") == 100
-    assert value("you good") == 100
+    assert value("what's happening") == 100
+    assert value("WHAT'S HAPPENING") == 100
 
 
 def test_case():
