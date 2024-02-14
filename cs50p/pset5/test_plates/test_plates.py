0584a6f43bb2883fec99b548432b91ae21209c73
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-14 01:19:59 +0000

    Wed, Feb 14, 2024, 9:19 AM +08:00

--- cs50p/pset5/test_plates/test_plates.py
+++ cs50p/pset5/test_plates/test_plates.py
@@ -1,3 +1,22 @@
 from plates import is_valid
 
 
+def test_starts_w_two_letters():
+    assert is_valid("CS50") == True
+    assert is_valid("A150") == False
+
+
+def test_max_chars():
+    assert is_valid("AAA222") == True
+    assert is_valid("A23456") == False
+    assert is_valid("AA123456") == False
+
+
+def test_number_in_middle():
+    assert is_valid("AAA22A") == False
+    assert is_valid("AA0123") == False
+
+
+def test_only_alnum():
+    assert is_valid("AAA.12") == False
+    assert is_valid("AA 123") == False
