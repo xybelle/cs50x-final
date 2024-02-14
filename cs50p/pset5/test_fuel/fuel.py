e0cc749f4fa2f40c6ca936580df2b4cd8a0ee2fe
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-14 01:22:11 +0000

    Wed, Feb 14, 2024, 9:22 AM +08:00

---
+++ cs50p/pset5/test_fuel/fuel.py
@@ -0,0 +1,28 @@
+def main():
+    percent = get_fraction()
+
+    if percent <= 1:
+        print("E")
+    elif percent >= 99:
+        print("F")
+    else:
+        print(f"{int(round(percent))}%")
+
+
+def get_fraction():
+    while True:
+        try:
+            fraction = input("Fraction: ").split('/')
+            percent = (int(fraction[0]) / int(fraction[1])) * 100
+            if percent > 100:
+                raise ExceedHundred
+            return percent
+        except (ValueError, ZeroDivisionError, ExceedHundred):
+            pass
+
+
+class ExceedHundred(Exception):
+    pass
+
+
+main()
