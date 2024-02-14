4120228be019a1f9c0e87d534e5f932fcc0e4156
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-14 01:41:27 +0000

    Wed, Feb 14, 2024, 9:41 AM +08:00

--- cs50p/pset5/test_fuel/fuel.py
+++ cs50p/pset5/test_fuel/fuel.py
@@ -1,37 +1,7 @@
 def main():
-    percent = get_fraction()
-
-    if percent <= 1:
-        print("E")
-    elif percent >= 99:
-        print("F")
-    else:
-        print(f"{int(round(percent))}%")
-
-
-def get_fraction():
-    while True:
-        try:
-            fraction = input("Fraction: ").split('/')
-            percent = (int(fraction[0]) / int(fraction[1])) * 100
-            if percent > 100:
-                raise ExceedHundred
-            return percent
-        except (ValueError, ZeroDivisionError, ExceedHundred):
-            pass
-
-
-class ExceedHundred(Exception):
-    pass
-
-
-if __name__ == "__main__"
-    main()
-
-
-def main():
     f = input("Fraction: ")
     convert(f)
+    print(gauge)
 
 
 def convert(fraction):
@@ -48,13 +18,13 @@ def convert(fraction):
 
 
 def gauge(percentage):
-    if percent <= 1:
-        print("E")
-    elif percent >= 99:
-        print("F")
+    if percentage <= 1:
+        return "E"
+    elif percentage >= 99:
+        return "F"
     else:
-        print(f"{percent}%")
+        return f"{percentage}%"
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     main()
