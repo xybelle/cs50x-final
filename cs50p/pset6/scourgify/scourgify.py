1c3753c8a7851517b0ea4f5fce63cf793df2e6e2
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-15 02:15:54 +0000

    Thu, Feb 15, 2024, 10:15 AM +08:00

--- cs50p/pset6/scourgify/scourgify.py
+++ cs50p/pset6/scourgify/scourgify.py
@@ -3,32 +3,36 @@ import sys
 
 from tabulate import tabulate
 
-
+# Ensure user prove two command-line arguments
 if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
 
-before = []
+raw_data = []
 
+# Error handling when reading CSV and appending raw_data dict
 try:
     with open(sys.argv[1], "r") as file:
         reader = csv.DictReader(file)
         for row in reader:
-            before.append(row)
+            raw_data.append(row)
 except FileNotFoundError:
     sys.exit(f"Could not read {sys.argv[1]}")
 
-after = []
+processed_data = []
 
-for row in before:
+# Splitting name into first and last name
+# Append first, last, house into processed_data dict
+for row in raw_data:
     sname, fname = row["name"].lstrip().split(",")
     house = row["house"]
-    after.append({"first name": fname, "last name": sname, "house": house})
+    processed_data.append({"first name": fname, "last name": sname, "house": house})
 
+# Writing new CSV
 with open(sys.argv[2], "a") as file:
     fieldnames = ["first name", "last name", "house"]
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()
-    for row in after:
+    for row in processed_data:
         writer.writerow(row)
