a314428a1011778d79cef12c6125f8cbc4cc53e2
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-15 17:52:20 +0000

    Fri, Feb 16, 2024, 1:52 AM +08:00

--- cs50p/pset6/shirt/shirt.py
+++ cs50p/pset6/shirt/shirt.py
@@ -6,6 +6,7 @@ from PIL import Image, ImageOps
 
 valid_extensions = [".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".PNG"]
 
+
 def main():
     # Ensure user provide two command-line arguments
     if len(sys.argv) < 3:
@@ -34,25 +35,6 @@ def resize_image():
         save_image(shirt, new_image)
     except FileNotFoundError:
         sys.exit(f"Cannot open {sys.argv[1]}")
-
-
-def valid():
-    # Ensure user provide two command-line arguments
-    if len(sys.argv) < 3:
-        print("Too few command-line arguments\nUsage: python shirt.py input output")
-        return False
-    elif len(sys.argv) > 3:
-        print("Too many command-line arguments\nUsage: python shirt.py input output")
-        return False
-    temp_a = os.path.splitext(sys.argv[1])
-    temp_b = os.path.splitext(sys.argv[2])
-    if temp_a[1] != temp_b[1]:
-        print("Input and output have different extensions")
-        return False
-    if temp_a[1] not in valid_extensions and temp_b[1] not in valid_extensions:
-        print("Invalid input or output extension(s)")
-        return False
-    return True
 
 
 def save_image(shirt, new_image):
