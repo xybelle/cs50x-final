b479359f1081d9f7ed1ad62bb6529328f9341003
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-15 02:59:54 +0000

    Thu, Feb 15, 2024, 10:59 AM +08:00

--- cs50p/pset6/shirt/shirt.py
+++ cs50p/pset6/shirt/shirt.py
@@ -2,13 +2,17 @@ import sys
 
 def main():
     # Ensure user provide two command-line arguments
-     if len(sys.argv) < 3:
-        sys.exit("Too few command-line arguments")
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments\nUsage: python shirt.py input output")
     elif len(sys.argv) > 3:
-        sys.exit("Too many command-line arguments")
+        sys.exit("Too many command-line arguments\nUsage: python shirt.py input output")
 
     temp1 = sys.argv[1].split(".")
     temp2 = sys.argv[2].split(".")
 
     if temp1[1] != temp2[1]:
         sys.exit("Input and output have different extensions")
+
+
+if __name__ == "__main__":
+    main()
