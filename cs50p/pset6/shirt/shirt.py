cb2f289d59a4a22eae01ec375be325de10b0de84
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-15 04:21:54 +0000

    Thu, Feb 15, 2024, 12:21 PM +08:00

--- cs50p/pset6/shirt/shirt.py
+++ cs50p/pset6/shirt/shirt.py
@@ -15,7 +15,7 @@ def main():
 def resize_image():
     shirt = Image.open("shirt.png")
     size = shirt.size
-    new_image = ImageOps.fit(sys.argv[2], size=(size[0], size[1]))
+    new_image = ImageOps.fit(sys.argv[2], size=size)
     save_image(shirt, new_image)
     # Resize input image
     #with Image.open(sys.argv[1]) as im:
