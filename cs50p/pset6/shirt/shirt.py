aafafa525bb45015dea140a63758033b1b7d6fd4
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-15 03:10:09 +0000

    Thu, Feb 15, 2024, 11:10 AM +08:00

--- cs50p/pset6/shirt/shirt.py
+++ cs50p/pset6/shirt/shirt.py
@@ -13,18 +13,21 @@ def valid():
     # Ensure user provide two command-line arguments
     if len(sys.argv) < 3:
         message = "Too few command-line arguments\nUsage: python shirt.py input output"
+        return message
     elif len(sys.argv) > 3:
         message = "Too many command-line arguments\nUsage: python shirt.py input output"
-
+        return message
     temp1 = sys.argv[1].split(".")
     temp2 = sys.argv[2].split(".")
 
     if temp1[1] != temp2[1]:
         message = "Input and output have different extensions"
+        return message
     if temp1[1] not in valid_extensions and temp2[1] not in valid_extensions:
         message = "Invalid input or output extension(s)"
+        return message
 
-    return message
+    return True
 
 
 if __name__ == "__main__":
