9b8f5496405f779d966a9be249cdf7c32ac04113
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-15 03:49:39 +0000

    Thu, Feb 15, 2024, 11:49 AM +08:00

--- cs50p/pset6/shirt/shirt.py
+++ cs50p/pset6/shirt/shirt.py
@@ -1,16 +1,15 @@
 import os
 import sys
 
-valid_extensions = [".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".PNG"]
+from PIL import Image
 
+
+valid_extensions = [".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".PNG"]
 
 def main():
     if not valid():
         sys.exit()
     #get_images()
-    temp1 = os.path.splitext(sys.argv[1])
-    print(temp1)
-    print(temp1['ext'])
 
 
 def get_images():
@@ -27,14 +26,14 @@ def valid():
     elif len(sys.argv) > 3:
         print("Too many command-line arguments\nUsage: python shirt.py input output")
         return False
-    #temp1 = splitext(sys.argv[1])
-    #temp2 = splitext(sys.argv[2])
-    #if temp1[1] != temp2[1]:
-        #print("Input and output have different extensions")
-        #return False
-    #if temp1[1] not in valid_extensions and temp2[1] not in valid_extensions:
-        #print("Invalid input or output extension(s)")
-        #return False
+    temp_a = os.path.splitext(sys.argv[1])
+    temp_b = os.path.splitext(sys.argv[2])
+    if temp_a[1] != temp_b[1]:
+        print("Input and output have different extensions")
+        return False
+    if temp_a[1] not in valid_extensions and temp_b[1] not in valid_extensions:
+        print("Invalid input or output extension(s)")
+        return False
     return True
 
 
