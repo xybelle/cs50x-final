f1b21df6c84147f55d93af774b8724e9585f4e54
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-15 03:13:02 +0000

    Thu, Feb 15, 2024, 11:13 AM +08:00

--- cs50p/pset6/shirt/shirt.py
+++ cs50p/pset6/shirt/shirt.py
@@ -6,27 +6,25 @@ valid_extensions = ["jpg", "JPG", "jpeg", "JPEG", "png", "PNG"]
 
 def main():
     if not valid():
-        sys.exit(f"{message}")
+        sys.exit()
 
 
 def valid():
     # Ensure user provide two command-line arguments
     if len(sys.argv) < 3:
-        message = "Too few command-line arguments\nUsage: python shirt.py input output"
-        return message
+        print("Too few command-line arguments\nUsage: python shirt.py input output")
+        return False
     elif len(sys.argv) > 3:
-        message = "Too many command-line arguments\nUsage: python shirt.py input output"
-        return message
+        print("Too many command-line arguments\nUsage: python shirt.py input output")
+        return False
     temp1 = sys.argv[1].split(".")
     temp2 = sys.argv[2].split(".")
-
     if temp1[1] != temp2[1]:
-        message = "Input and output have different extensions"
-        return message
+        print("Input and output have different extensions")
+        return False
     if temp1[1] not in valid_extensions and temp2[1] not in valid_extensions:
-        message = "Invalid input or output extension(s)"
-        return message
-
+        print("Invalid input or output extension(s)")
+        return False
     return True
 
 
