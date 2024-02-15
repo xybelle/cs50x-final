dcdafaf99ad9cdac16d5abc42fdbe56cc108758a
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-15 02:11:33 +0000

    Thu, Feb 15, 2024, 10:11 AM +08:00

--- cs50p/pset6/scourgify/scourgify.py
+++ cs50p/pset6/scourgify/scourgify.py
@@ -8,8 +8,6 @@ if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
-#elif ".csv" not in sys.argv[1] and ".csv" not in sys.arg[2]:
-#    sys.exit("Not a CSV file")
 
 before = []
 
@@ -24,12 +22,9 @@ except FileNotFoundError:
 after = []
 
 for row in before:
-    sname, fname  = row["name"].lstrip().split(",")
+    sname, fname = row["name"].lstrip().split(",")
     house = row["house"]
     after.append({"first name": fname, "last name": sname, "house": house})
-
-#print(tabulate(after, headers="keys", tablefmt="grid"))
-
 
 with open(sys.argv[2], "a") as file:
     fieldnames = ["first name", "last name", "house"]
