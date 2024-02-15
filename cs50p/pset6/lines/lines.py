88fd6becb9e34d631d570261411f2935f9bbf5f9
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-15 00:44:44 +0000

    Thu, Feb 15, 2024, 8:44 AM +08:00

--- cs50p/pset6/lines/lines.py
+++ cs50p/pset6/lines/lines.py
@@ -7,4 +7,5 @@ elif len(sys.argv) > 2:
     sys.exit("Too many command-line arguments")
 elif ".py" not in sys.argv[1]:
     sys.exit("Not a Python file")
-
+elif sys.argv[1] is None:
+    sys.exit("File does not exist")
