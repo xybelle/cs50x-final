d7d99ecbe7f52d890d9ada7e46e1cb586cf8b611
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-16 01:11:29 +0000

    Fri, Feb 16, 2024, 9:11 AM +08:00

--- cs50p/pset7/numb3rs/numb3rs.py
+++ cs50p/pset7/numb3rs/numb3rs.py
@@ -6,22 +6,27 @@ def main():
 
 
 def validate(ip):
-    if not ip.isdigit():
-        return False
-    parts = ip.split(".")
-    if len(parts) > 4:
+    sets = ip.split(".")
+    if not sets.isdigit():
         return False
     if valid := re.search(r"([0-2]?[0-9]?[0-9]?\.){3}[0-2]?[0-9]?[0-9]?", ip):
-        if max_255(parts):
+        if max_255(ip) and four_sets(ip):
             return True
     return False
 
 
 def max_255(digits):
+    digits = digits.split(".")
     for digit in digits:
         if int(digit) > 255:
             return False
             break
+    return True
+
+
+def four_sets():
+    if len(sets) > 4:
+        return False
     return True
 
 
