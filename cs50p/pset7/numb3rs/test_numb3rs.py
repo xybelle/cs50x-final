57bb4324c9251160814e0fa177875db16db183ef
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-16 02:17:53 +0000

    Fri, Feb 16, 2024, 10:17 AM +08:00

--- cs50p/pset7/numb3rs/test_numb3rs.py
+++ cs50p/pset7/numb3rs/test_numb3rs.py
@@ -1,16 +1,19 @@
-from numb3rs
+from numb3rs import validate
 
 
-def test_validate():
-    assert numb3rs.validate("1.2.3.4") == True
-    assert numb3rs.validate("255.255.255.255") == True
-    assert numb3rs.validate("275.1.1.1") == False
-    assert numb3rs.validate("cat") == False
-    assert numb3rs.validate("255.276.255.255") == False
-    assert numb3rs.validate("255.255.265.255") == False
-    assert numb3rs.validate("255.255.255.285") == False
+def test_in_range():
+    assert validate("1.2.3.4") == True
+    assert validate("255.255.255.255") == True
+    assert validate("275.1.1.1") == False
+    assert validate("255.276.255.255") == False
+    assert validate("255.255.265.255") == False
+    assert validate("255.255.255.285") == False
 
 
 def test_four_sets():
-    assert numb3rs.validate("1.1.1.1.1") == False
-    assert numb3rs.validate("1.1.1.1") == True
+    assert validate("1.1.1.1.1") == False
+    assert validate("1.1.1.1") == True
+
+
+def test_only_digits():
+    assert validate("cat") == False
