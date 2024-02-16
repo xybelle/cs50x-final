e503e5c9942aec83885018391b2417e0bd904f93
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-16 02:18:31 +0000

    Fri, Feb 16, 2024, 10:18 AM +08:00

--- cs50p/pset7/numb3rs/numb3rs.py
+++ cs50p/pset7/numb3rs/numb3rs.py
@@ -13,17 +13,6 @@ def validate(ip):
         return False
 
 
-#def max_255(digits):
-#    digits = digits.split(".")
-#    for digit in digits:
-#        if not digit.isdigit():
-#            return False
-#        if int(digit) > 255:
-#            return False
-#            break
-#    return True
-
-
 def four_sets(n):
     n = n.split(".")
     if len(n) != 4:
