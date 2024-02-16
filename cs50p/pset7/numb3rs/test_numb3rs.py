08e1ec593fa3147ba868f678ab8a9674f6d99e06
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-16 01:26:04 +0000

    Fri, Feb 16, 2024, 9:26 AM +08:00

--- cs50p/pset7/numb3rs/test_numb3rs.py
+++ cs50p/pset7/numb3rs/test_numb3rs.py
@@ -7,7 +7,7 @@ def test_validate():
     assert validate("cat") == False
 
 
-def test_max_255():
+def test_validate():
     assert max_255("256.255.255.255") == False
     assert max_255("255.255.255.255") == True
     assert max_255("1.1.1.1") == True
