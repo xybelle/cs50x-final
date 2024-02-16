fbcd1c2d3b4d0f90139d077b0dd4046f64a06598
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-16 00:16:02 +0000

    Fri, Feb 16, 2024, 8:16 AM +08:00

--- cs50p/pset7/numb3rs/numb3rs.py
+++ cs50p/pset7/numb3rs/numb3rs.py
@@ -6,11 +6,16 @@ def main():
 
 
 def validate(ip):
-    re.search(r"(0-2)?(0-5)?(0-5)?\.")
+    n = ip.strip(".")
+    if max_255(n):
+        re.search(r"[0-2]?[0-9]?[0-9]?\.")
 
 
-
-...
+def max_255(num):
+    for _ in num:
+        if int(num) > 255:
+            return False
+    return True
 
 
 if __name__ == "__main__":
