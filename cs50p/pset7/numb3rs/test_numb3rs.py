18825b16eba0dd81fedee83aacc7875f6c7c6d69
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-16 02:15:20 +0000

    Fri, Feb 16, 2024, 10:15 AM +08:00

--- cs50p/pset7/numb3rs/test_numb3rs.py
+++ cs50p/pset7/numb3rs/test_numb3rs.py
@@ -1,4 +1,4 @@
-import numb3rs
+from numb3rs
 
 
 def test_validate():
@@ -6,9 +6,9 @@ def test_validate():
     assert numb3rs.validate("255.255.255.255") == True
     assert numb3rs.validate("275.1.1.1") == False
     assert numb3rs.validate("cat") == False
-
-
-
+    assert numb3rs.validate("255.276.255.255") == False
+    assert numb3rs.validate("255.255.265.255") == False
+    assert numb3rs.validate("255.255.255.285") == False
 
 
 def test_four_sets():
