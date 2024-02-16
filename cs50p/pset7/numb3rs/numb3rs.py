ea49ad01fa23b25ad549bd310ccf4a40dd487fee
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-16 02:02:19 +0000

    Fri, Feb 16, 2024, 10:02 AM +08:00

--- cs50p/pset7/numb3rs/numb3rs.py
+++ cs50p/pset7/numb3rs/numb3rs.py
@@ -7,20 +7,20 @@ def main():
 
 def validate(ip):
     if valid := re.search(r"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?", ip):
-        if max_255(ip) and four_sets(ip):
+        if four_sets(ip):
             return True
     return False
 
 
-def max_255(digits):
-    digits = digits.split(".")
-    for digit in digits:
-        if not digit.isdigit():
-            return False
-        if int(digit) > 255:
-            return False
-            break
-    return True
+#def max_255(digits):
+#    digits = digits.split(".")
+#    for digit in digits:
+#        if not digit.isdigit():
+#            return False
+#        if int(digit) > 255:
+#            return False
+#            break
+#    return True
 
 
 def four_sets(n):
--- cs50p/pset7/numb3rs/test_numb3rs.py
+++ cs50p/pset7/numb3rs/test_numb3rs.py
@@ -8,10 +8,7 @@ def test_validate():
     assert numb3rs.validate("cat") == False
 
 
-def test_max_255():
-    assert numb3rs.max_255("256.255.255.255") == False
-    assert numb3rs.max_255("255.255.255.255") == True
-    assert numb3rs.max_255("1.1.1.1") == True
+
 
 
 def test_four_sets():
