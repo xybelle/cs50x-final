06f34a20bb7382cd51f89f4f2d7e3078562b288c
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-16 02:03:35 +0000

    Fri, Feb 16, 2024, 10:03 AM +08:00

--- cs50p/pset7/numb3rs/numb3rs.py
+++ cs50p/pset7/numb3rs/numb3rs.py
@@ -9,7 +9,8 @@ def validate(ip):
     if valid := re.search(r"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?", ip):
         if four_sets(ip):
             return True
-    return False
+    else:
+        return False
 
 
 #def max_255(digits):
