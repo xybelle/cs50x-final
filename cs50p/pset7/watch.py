131e128ce97efa75bedb1800d7e192c79655a826
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-17 03:18:41 +0000

    Sat, Feb 17, 2024, 11:18 AM +08:00

--- cs50p/pset7/watch.py
+++ cs50p/pset7/watch.py
@@ -6,7 +6,7 @@ def main():
 
 
 def parse(s):
-    if found := re.search(r".+src=\"https?://(?:www\.)?youtube\.com/embed/(.+)\">? title.+", s):
+    if found := re.search(r".+src=\"https?://(?:www\.)?youtube\.com/embed/(.+)\">? title.+?", s):
         #return found.groups(1)
         vid_id = found.groups(1)
         print(vid_id)
