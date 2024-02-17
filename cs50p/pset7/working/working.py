3ed424f222e666d2c48c818f58a50c3c288aec9a
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-17 03:55:24 +0000

    Sat, Feb 17, 2024, 11:55 AM +08:00

--- cs50p/pset7/working/working.py
+++ cs50p/pset7/working/working.py
@@ -8,7 +8,7 @@ def main():
 
 def convert(s):
     try:
-        if matches := (re.search(r"\d:?\d?\d? [A|P|]M to \d:?\d?\d? [A|P|]M", s, re.IGNORECASE)):
+        if matches := (re.search(r"(\d:?\d?\d?) ([A|P|]M) to (\d:?\d?\d?) ([A|P|]M)", s, re.IGNORECASE)):
             print(matches.groups())
     except ValueError:
         sys.exit("Hours: time AM/PM to time AM/PM")
