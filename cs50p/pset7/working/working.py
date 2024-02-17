079f54d9375dcd55e79c7858416ce816ca41e4f9
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-17 04:16:37 +0000

    Sat, Feb 17, 2024, 12:16 PM +08:00

--- cs50p/pset7/working/working.py
+++ cs50p/pset7/working/working.py
@@ -7,10 +7,7 @@ def main():
 
 
 def convert(s):
-    valid_time = validate_input(s)
-
-
-def validate_input(s):
+    #valid_time = validate_input(s)
     try:
         if matches := re.search(r"((1[0-2]|1-9]):?[0-5][0-9]?) (AM|PM) to (1[0-2]|1-9]):?[0-5][0-9]?) (AM|PM)", s):
             print(matches.groups())
@@ -18,6 +15,9 @@ def validate_input(s):
             raise ValueError
     except ValueError:
         sys.exit("Hours: time AM/PM to time AM/PM")
+
+#def validate_input(s):
+
 
 
 if __name__ == "__main__":
