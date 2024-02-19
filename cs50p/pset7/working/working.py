794854d1abdcfdf46214ab02b5bab83fef5c6b22
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-19 03:31:08 +0000

    Mon, Feb 19, 2024, 11:31 AM +08:00

--- cs50p/pset7/working/working.py
+++ cs50p/pset7/working/working.py
@@ -28,7 +28,7 @@ def convert(s):
 
 def validate_input(s):
     try:
-        if matches := re.search(r"((?:1[0-2]|[1-9]):?(?:[0-5][0-9])?) (AM|PM) to ((?:1[0-2]|[1-9]):?(?:[0-5][0-9])?) (AM|PM)", s):
+        if matches := re.search(r"((?:1[0-2]|[1-9])(?::[0-5][0-9])?) (AM|PM) to ((?:1[0-2]|[1-9])(?::[0-5][0-9])?) (AM|PM)", s):
             return matches.groups()
         else:
             raise ValueError
