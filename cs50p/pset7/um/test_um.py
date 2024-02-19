9c2e7da6b05a2247eed151764f766ea284f44638
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-19 14:30:35 +0000

    Mon, Feb 19, 2024, 10:30 PM +08:00

--- cs50p/pset7/um/test_um.py
+++ cs50p/pset7/um/test_um.py
@@ -7,6 +7,13 @@ def test_um():
     assert count("um, hello, um, world") == 2
     assert count("yummy") == None
     assert count("um?") == 1
+
+
+def test_case_insensitive():
     assert count("Um, thanks for the album") == 1
     assert count("Um, thanks, um...") == 2
+
+
+def test_spaces_around():
     assert count("um, um are you sure?") == 2
+
