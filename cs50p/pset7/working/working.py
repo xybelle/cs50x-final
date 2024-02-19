8ad7d71ebf78cb6ea34e4a983c080282c40a8096
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-19 00:42:54 +0000

    Mon, Feb 19, 2024, 8:42 AM +08:00

--- cs50p/pset7/working/working.py
+++ cs50p/pset7/working/working.py
@@ -16,8 +16,15 @@ def convert(s):
             h1 = int(h1) + 12
     elif first := re.search(r"(?:1[0-2]|[1-9])", valid_time[0]):
         h1 = int(valid_time[0]), m1 = 0
+
     if second := re.search(r"(?:1[0-2]|[1-9]):([0-5][0-9]))", valid_time[2]):
-        int(h1), int(m1) = valid_time[2].split(":")
+        h2, m2 = valid_time[2].split(":")
+        if h2 == "12":
+            h2 = 0
+        else:
+            h2 = int(h2) + 12
+    elif second := re.search(r"(?:1[0-2]|[1-9])", valid_time[2]):
+        h2 = int(valid_time[2]), m2 = 0
 
     return f"{h1:02}:{m1:02} to {h2:02}:{m2:02}"
 
