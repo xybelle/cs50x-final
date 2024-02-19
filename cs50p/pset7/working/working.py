8c73ce928d6c5b6cce3058689cdf61b345f8912d
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-19 02:48:16 +0000

    Mon, Feb 19, 2024, 10:48 AM +08:00

--- cs50p/pset7/working/working.py
+++ cs50p/pset7/working/working.py
@@ -20,7 +20,6 @@ def convert(s):
     elif re.search(r"(?:1[0-2]|[1-9])", valid_time[2]):
         h2 = hours_fmt(valid_time[2], valid_time[3])
         m2 = 0
-
     return f"{h1:02}:{m1:02} to {h2:02}:{m2:02}"
 
 
