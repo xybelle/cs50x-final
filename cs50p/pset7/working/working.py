3c30191c7683c535a7ac575a35c4986dd3c2500f
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-19 00:31:21 +0000

    Mon, Feb 19, 2024, 8:31 AM +08:00

--- cs50p/pset7/working/working.py
+++ cs50p/pset7/working/working.py
@@ -8,14 +8,12 @@ def main():
 
 def convert(s):
     valid_time = validate_input(s)
-    if valid_time[1] == "PM":
-        h1, m1 = for_pm(valid_time[0])
-    elif valid_time[1] == "AM":
-        h1, m1 = for_am(valid_time[0])
-    if valid_time[3] == "PM":
-        h2, m2 = for_pm(valid_time[2])
-    elif valid_time[3] == "AM":
-        h2, m2 = for_am(valid_time[2])
+    if first := re.search(r"(?:1[0-2]|[1-9]):([0-5][0-9]))", valid_time[0]):
+        int(h1), int(m1) = valid_time[0].split(":")
+    elif first := re.search(r"(?:1[0-2]|[1-9])")
+    if second := re.search(r"(?:1[0-2]|[1-9]):([0-5][0-9]))", valid_time[2]):
+        int(h1), int(m1) = valid_time[2].split(":")
+
     return f"{h1:02}:{m1:02} to {h2:02}:{m2:02}"
 
 
@@ -29,47 +27,9 @@ def validate_input(s):
         sys.exit("Hours: time AM/PM to time AM/PM")
 
 
-def for_pm(time):
-    if ":" in time:
-            h, m = time.split(":")
-            if h == "12":
-                h == 12
-                return h, m
-            else:
-                h = int(h) + 12
-                return h, int(m)
-    else:
-        if time == "12":
-            h = 12
-            m = 0
-            return h, m
-        else:
-            h = int(time) + 12
-            m = 0
-            return h, m
-
-
-def for_am(time):
-    if ":" in time:
-        h, m = time.split(":")
-        if h == "12":
-            h = 0
-            return h, m
-        else:
-            return int(h), int(m)
-    else:
-        if time == "12":
-            h = 0
-            m = 0
-            return h, m
-        else:
-            h = int(time)
-            m = 0
-            return h, m
-
-
 if __name__ == "__main__":
     main()
 
 
-if matches := research(r"(?:1[0-2]|[1-9]):?([0-5][0-9])?)")
+
+
