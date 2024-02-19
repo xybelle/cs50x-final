cb50e44c3d7c457fc8a0e3ee75a86e3e62ffdfe6
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-19 14:25:57 +0000

    Mon, Feb 19, 2024, 10:25 PM +08:00

--- cs50p/pset7/um/test_um.py
+++ cs50p/pset7/um/test_um.py
@@ -5,4 +5,7 @@ from um import count
 
 def test_um():
     assert count("um, hello, um, world") == 2
-    assert count("yummy") == 0
+    assert count("yummy") == None
+    assert count("um?") == 1
+    assert count("Um, thanks for the album") == 1
+    assert count("Um, thnks, um...") == 2
