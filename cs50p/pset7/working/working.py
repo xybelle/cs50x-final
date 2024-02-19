08762a07c9b5bad11ad9c14896b8a6a7b640e818
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-19 01:04:50 +0000

    Mon, Feb 19, 2024, 9:04 AM +08:00

--- cs50p/pset7/working/working.py
+++ cs50p/pset7/working/working.py
@@ -38,13 +38,12 @@ def convert(s):
 
 
 def validate_input(s):
-    try:
+
         if matches := re.search(r"((?:1[0-2]|[1-9]):?([0-5][0-9])?) (AM|PM) to ((?:1[0-2]|[1-9]):?([0-5][0-9])?) (AM|PM)", s):
             return matches.groups()
         else:
             raise ValueError
-    except ValueError:
-        pass
+    
 
 
 if __name__ == "__main__":
