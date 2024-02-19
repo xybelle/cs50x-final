c8caa1e4718d253436129b59e5e34d91341ecb76
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-19 14:41:55 +0000

    Mon, Feb 19, 2024, 10:41 PM +08:00

--- cs50p/pset7/response/response.py
+++ cs50p/pset7/response/response.py
@@ -1,6 +1,13 @@
+from validator_collection import checkers, errors
+
+
 def main():
     print(validate(input("What's your email address?")))
 
 
 def validate(email):
-    
+    return checker.is_email(email)
+
+
+if __name__ == "__main__":
+    main()
