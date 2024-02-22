55203a7f4e8552a98b97b5ddfa299e20a20b5e90
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-22 10:37:02 +0000

    Thu, Feb 22, 2024, 6:37 PM +08:00

--- cs50p/pset8/seasons/seasons.py
+++ cs50p/pset8/seasons/seasons.py
@@ -1,12 +1,9 @@
 from datetime import date
-
+from inflect import
 
 def main():
     dob = input("Date of Birth: ")
-    print(age)
-
-
-def get_age():
+    print(number_to_words(dob))
 
 
 if __name__ == "__main__":
