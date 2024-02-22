7b245423503e51012cdcc5d2664cbba8f95fc41e
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-22 11:08:48 +0000

    Thu, Feb 22, 2024, 7:08 PM +08:00

--- cs50p/pset8/seasons/seasons.py
+++ cs50p/pset8/seasons/seasons.py
@@ -8,16 +8,17 @@ from datetime import date
 
 def main():
     dob = date.fromisoformat(input("Date of Birth: "))
-    age = get_age(dob)
+    print(get_age(dob))
 
 
 def get_age(dob):
     age = date.today() - dob
+    return age
 
 
-def sing(age):
-    s = p.number_to_words(dob)
-    return s.replace("and ", "")
+#def sing(age):
+#    s = p.number_to_words(dob)
+#    return s.replace("and ", "")
 
 
 if __name__ == "__main__":
