2c9fe915415d643c4d8873595b6b4b2d988c3871
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-22 12:38:58 +0000

    Thu, Feb 22, 2024, 8:38 PM +08:00

--- cs50p/pset8/jar/jar.py
+++ cs50p/pset8/jar/jar.py
@@ -1,2 +1,20 @@
 class Jar:
-    def __init__(self, capacity)
+    def __init__(self, capacity=0)
+        ...
+
+    def __str__(self):
+        ...
+
+    def deposit(self, n):
+        ...
+
+    def withdraw(self, n):
+        ...
+
+    @property
+    def capacity(self):
+        ...
+
+    @property
+    def size(self):
+        ...
