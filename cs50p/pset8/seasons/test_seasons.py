1aef31c20fd515323531409aaa54d97be9e91565
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-22 11:34:18 +0000

    Thu, Feb 22, 2024, 7:34 PM +08:00

--- cs50p/pset8/seasons/test_seasons.py
+++ cs50p/pset8/seasons/test_seasons.py
@@ -1,5 +1,11 @@
 import seasons
+import pytest
 
 
-def test_seasons():
+def test_age():
     assert  age("525600") == "Five hundred twenty-five thousand, six hundred minutes"
+
+
+def test_format():
+    with pytest.raises(ValueError):
+        main("1989-12")
