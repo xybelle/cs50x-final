11c507d4e50ddeefa55783ae4e8c2e0cc9ce14d3
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-22 12:22:32 +0000

    Thu, Feb 22, 2024, 8:22 PM +08:00

--- cs50p/pset8/seasons/test_seasons.py
+++ cs50p/pset8/seasons/test_seasons.py
@@ -2,10 +2,14 @@ import seasons
 import pytest
 
 
-def test_age():
+def test_sing():
     assert seasons.sing("525600") == "five hundred twenty-five thousand, six hundred"
+    assert seasons.sing("1051200") == "one million, fifty-one thousand, two hundred"
 
 
 def test_format():
     with pytest.raises(ValueError):
         seasons.get_dob("1989-12")
+
+
+
