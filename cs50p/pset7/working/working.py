7171ae4c68cffc8ab7f17a20173f0aafc182463f
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-22 09:31:20 +0000

    Thu, Feb 22, 2024, 5:31 PM +08:00

--- cs50p/pset7/working/working.py
+++ cs50p/pset7/working/working.py
@@ -8,8 +8,6 @@ def main():
 
 def convert(s):
     valid_time = validate_input(s)
-    #print(valid_time)
-    #print(valid_time[0])
     if re.search(r"(?:1[0-2]|[1-9]):[0-5][0-9]", valid_time[0]):
         h, m1 = valid_time[0].split(":")
         h1 = hours_mins_fmt(h, valid_time[1])
@@ -22,7 +20,6 @@ def convert(s):
     elif re.search(r"(?:1[0-2]|[1-9])", valid_time[2]):
         h2 = hours_fmt(valid_time[2], valid_time[3])
         m2 = 0
-    #print(f"{h1:02}:{m1:02} to {h2:02}:{m2:02}")
     return f"{h1:02}:{m1:02} to {h2:02}:{m2:02}"
 
 
@@ -30,10 +27,7 @@ def validate_input(s):
     try:
         if matches := re.search(r"((?:1[0-2]|[1-9])(?::[0-5][0-9])?) (AM|PM) to ((?:1[0-2]|[1-9])(?::[0-5][0-9])?) (AM|PM)", s):
             return matches.groups()
-        else:
-            raise ValueError
     except ValueError:
-        #print("Hour: time AM/PM to time AM/PM")
         sys.exit(ValueError)
 
 
