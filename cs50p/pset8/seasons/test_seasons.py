b1805bcb500c6781fff25bb6e5335904121d41e8
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-22 11:59:28 +0000

    Thu, Feb 22, 2024, 7:59 PM +08:00

--- cs50p/pset8/seasons/test_seasons.py
+++ cs50p/pset8/seasons/test_seasons.py
@@ -3,9 +3,9 @@ import pytest
 
 
 def test_age():
-    assert seasons.sing("525600") == "five hundred twenty-five thousand, six hundred minutes"
+    assert seasons.sing("525600") == "five hundred twenty-five thousand, six hundred"
 
 
 def test_format():
     with pytest.raises(ValueError):
-        seasons.main("1989-12")
+        seasons.get_age("1989-12")
