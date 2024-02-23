f022557806632872884f41d4e155e95d37766b4f
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-23 07:22:42 +0000

    Fri, Feb 23, 2024, 3:22 PM +08:00

--- cs50p/pset8/shirtificate/shirtificate.py
+++ cs50p/pset8/shirtificate/shirtificate.py
@@ -7,7 +7,7 @@ def main():
     pdf.add_page()
     pdf.set_font('helvetica', size=12)
     pdf.image("shirtificate.png", x=0, y=60)
-    pdf.cell(text="CS50 Shirtificate", text_align="Align")
+    pdf.cell(50, 50, text="CS50 Shirtificate")
     pdf.output("shirtificate.pdf")
 
 
