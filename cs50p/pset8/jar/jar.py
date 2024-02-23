9c44cc2724b23efe219e4ed67e1120d0f46961b3
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-23 00:54:12 +0000

    Fri, Feb 23, 2024, 8:54 AM +08:00

--- cs50p/pset8/jar/jar.py
+++ cs50p/pset8/jar/jar.py
@@ -14,7 +14,13 @@ class Jar:
 
     @property
     def capacity(self):
-        ...
+        return self._capacity
+
+    @capacity.setter
+    def capacity(self, capacity):
+        if capacity <= 0:
+            raise ValueError("Capacity must be greater than zero")
+        self._capacity = capacity
 
     @property
     def size(self):
