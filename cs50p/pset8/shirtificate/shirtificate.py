c3fe595105108bcb2d5b27a0af1242e60139a06a
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-23 07:14:40 +0000

    Fri, Feb 23, 2024, 3:14 PM +08:00

--- cs50p/pset8/shirtificate/shirtificate.py
+++ cs50p/pset8/shirtificate/shirtificate.py
@@ -1,8 +1,19 @@
 from fpdf import FPDF
 
-pdf = FPDF()
-pdf.add_page()
-pdf.set_font('helvetica', size=12)
-pdf.image("shirtificate.png", x=20, y=60)
-pdf.cell(text="hello world")
-pdf.output("shirtificate.pdf")
+
+def main():
+    name = get_name()
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('helvetica', size=12)
+    pdf.image("shirtificate.png", x=20, y=60)
+    pdf.cell(text="CS50 Shirtificate")
+    pdf.output("shirtificate.pdf")
+
+
+def get_name():
+    return input("Name: ")
+
+
+if __name__ == "__main__":
+    main()
