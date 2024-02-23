0a30f74dc7f4d4f5110cc4335244e9538c0915e7
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-23 01:51:26 +0000

    Fri, Feb 23, 2024, 9:51 AM +08:00

--- cs50p/pset8/jar/test_jar.py
+++ cs50p/pset8/jar/test_jar.py
@@ -1,11 +1,15 @@
+import pytest
+
 from jar import Jar
 
 
 def test_init():
     jar = Jar(1)
     assert jar.capacity == 1
-    jar = Jar(0)
-    
+    jar = Jar(10)
+    assert jar.capacity == 10
+    with pytest.raises(ValueError):
+        jar = Jar(0)
 
 
 def test_str():
@@ -15,3 +19,20 @@ def test_str():
     assert str(jar) == "🍪"
     jar.deposit(11)
     assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+
+
+def test_deposit():
+    jar = Jar()
+    jar.deposit(1)
+    assert jar.size == 1
+    with pytest.raises(ValueError):
+        jar.deposit(12)
+
+
+def test_withdraw():
+    jar = Jar()
+    jar.deposit(10)
+    jar.withdraw(5)
+    assert jar.size == 5
+    with pytest.raises(ValueError):
+        jar.withdraw(6)
