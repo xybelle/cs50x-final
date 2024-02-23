e9a5645c3c30d3e560cc83fab0d86b38049e1bdf
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-23 07:35:21 +0000

    Fri, Feb 23, 2024, 3:35 PM +08:00

--- cs50p/pset8/shirtificate/shirtificate.py
+++ cs50p/pset8/shirtificate/shirtificate.py
@@ -5,9 +5,9 @@ def main():
     name = get_name()
     pdf = FPDF()
     pdf.add_page()
-    pdf.set_font('helvetica', size=30)
+    pdf.set_font("helvetica", size=30)
     pdf.image("shirtificate.png", x=0, y=60)
-    pdf.cell(border=0, align=Align.C, text="CS50 Shirtificate")
+    pdf.cell(border=0, align="C", text="CS50 Shirtificate")
     pdf.output("shirtificate.pdf")
 
 
