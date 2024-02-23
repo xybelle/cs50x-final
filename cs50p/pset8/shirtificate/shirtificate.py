6c2ee55034ba3b630f7c406de39f5e6b917a57d2
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-23 07:11:20 +0000

    Fri, Feb 23, 2024, 3:11 PM +08:00

--- cs50p/pset8/shirtificate/shirtificate.py
+++ cs50p/pset8/shirtificate/shirtificate.py
@@ -3,5 +3,6 @@ from fpdf import FPDF
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font('helvetica', size=12)
+pdf.image("shirtificate.png", x=20, y=60)
 pdf.cell(text="hello world")
-pdf.output("hello_world.pdf")
+pdf.output("shirtificate.pdf")
