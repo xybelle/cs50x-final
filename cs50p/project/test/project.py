2eeab308a407ec24d4f211f1a3f0a19ff6ce9dc1
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-24 02:52:47 +0000

    Sat, Feb 24, 2024, 10:52 AM +08:00

---
+++ cs50p/project/test/project.py
@@ -0,0 +1,21 @@
+import random
+
+def main():
+    # get arithmetic operation to practice
+    # get level
+
+
+def practice_addition():
+    ...
+
+
+def practice_subtraction():
+    ...
+
+
+def practice_multiplication():
+    ...
+
+
+def practice_division():
+    ...
