ba01b425199594145fd4edb6cd4d1bae86917c3c
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-26 09:16:30 +0000

    Mon, Feb 26, 2024, 5:16 PM +08:00

--- cs50p/project/test/test_game.py
+++ cs50p/project/test/test_game.py
@@ -5,5 +5,9 @@ from unittest.mock import patch
 
 def test_practice_addition():
     # Mocking input to provide correct answers
-    with patch('builtins.input', side_effect=['4','6', '8', '2', '5', '7', '9', '1', '3', '0']):
+    with patch('builtins.input', side_effect=[
+        '4', '6', '8', '2', '5', '7', '9', '1', '3', '0',
+        '4', '6', '8', '2', '5', '7', '9', '1', '3', '0',
+        '4', '6', '8', '2', '5', '7', '9', '1', '3', '0'
+        ]):
         assert game.practice_addition(1) == 10
