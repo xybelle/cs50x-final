ec9eee4da5c062eb248b9d98a9d65c737c54c4c1
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-26 04:31:26 +0000

    Mon, Feb 26, 2024, 12:31 PM +08:00

--- cs50p/project/project.py
+++ cs50p/project/project.py
@@ -27,7 +27,7 @@ def add_grade(student_id, test_id, grade):
     Allows teacher to add a grade for a specific student and assignment/test.
 
     :param student_id, test_id, grade
-    :type student_id: int, test_id: int, grade: float
+    :type student_id: str, test_id: str, grade: float
     :raise ValueError: if student_id not in db
     """
 
@@ -37,7 +37,7 @@ def get_average(student_id):
     Calculates the average grade for a specific student
 
     :param student_id: identifier
-    :type student_id: int
+    :type student_id: str
     :raise ValueError: If student_id not in db
     :return: Grade average
     :rtype: float
@@ -49,7 +49,7 @@ def generate_report(student_id):
     Generate report of all grades for a specific student
 
     :param student_id
-    :type student_id: int
+    :type student_id: str
     :raise ValueError: If student_id not in db
     :return: A csv file of grade
     """
