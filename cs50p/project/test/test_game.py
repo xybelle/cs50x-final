da7a805b3b094012c4a8fbdec0e39c06b30309ce
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-26 09:45:44 +0000

    Mon, Feb 26, 2024, 5:45 PM +08:00

--- cs50p/project/test/test_game.py
+++ cs50p/project/test/test_game.py
@@ -11,16 +11,26 @@ def mock_input(prompt):
     # Return a predictable value for testing
     return "2"
 
+
+# Mocking input function
+def mock_input_sub(prompt):
+    # Return a predictable value for testing
+    return "0"
+
+
 @patch('game.generate_integer', side_effect=mock_generate_integer)
 @patch('builtins.input', side_effect=mock_input)
 def test_practice_addition(mock_input, mock_generate_integer):
-    assert game.practice_addition(2) == 10  # Assuming level 2 requires 10 correct answers
+    assert game.practice_addition(2) == 10
 
 
 @patch('game.generate_integer', side_effect=mock_generate_integer)
-@patch('builtins.input', side_effect=mock_input)
+@patch('builtins.input', side_effect=mock_input_sub)
 def test_practice_subtraction(mock_input, mock_generate_integer):
-    assert game.practice_addition(2) == 10  # Assuming level 2 requires 10 correct answers
+    assert game.practice_subtraction(2) == 10
 
-if __name__ == "__main__":
-    test_practice_addition()
+
+@patch('game.generate_integer', side_effect=mock_generate_integer)
+@patch('builtins.input', side_effect=mock_input_sub)
+def test_practice_multi(mock_input, mock_generate_integer):
+    assert game.practice_multi(2) == 10
