02c2e301b079617d100ab9043a366ed15cafe65a
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-26 11:42:31 +0000

    Mon, Feb 26, 2024, 7:42 PM +08:00

--- cs50p/project/test_proj1.py
+++ cs50p/project/test_proj1.py
@@ -5,21 +5,31 @@ from unittest.mock import patch
 # Mocking generate_integer function
 def mock_generate_integer(n):
     # Return a predictable value for testing
-    return 1
+    return 3
 
 # Mocking input function for user input simulation
-def mock_input(prompt):
+def mock_input_six(prompt):
     # Return a predictable value for testing
-    return '2'  # Sample input for testing
+    return '6'  # Sample input for testing
 
 # Mocking input function for user input simulation
 def mock_input_zero(prompt):
     # Return a predictable value for testing
     return '0'  # Sample input for testing
 
+# Mocking input function for user input simulation
+def mock_input_nine(prompt):
+    # Return a predictable value for testing
+    return '9'  # Sample input for testing
+
+# Mocking input function for user input simulation
+def mock_input_one(prompt):
+    # Return a predictable value for testing
+    return '1'  # Sample input for testing
+
 @patch('proj1.generate_integer', side_effect=mock_generate_integer)
-@patch('builtins.input', side_effect=mock_input)
-def test_practice_arithmetic_addition(mock_input, mock_generate_integer):
+@patch('builtins.input', side_effect=mock_input_six)
+def test_practice_arithmetic_addition(mock_input_six, mock_generate_integer):
     # Testing practice_arithmetic function for addition
     assert proj1.practice_arithmetic(1, '+') == 10  # Assuming 10 correct answers for level 1
 
@@ -28,3 +38,15 @@ def test_practice_arithmetic_addition(mock_input, mock_generate_integer):
 def test_practice_arithmetic_subtraction(mock_input_zero, mock_generate_integer):
     # Testing practice_arithmetic function for subtraction
     assert proj1.practice_arithmetic(1, '-') == 10  # Assuming 10 correct answers for level 1
+
+@patch('proj1.generate_integer', side_effect=mock_generate_integer)
+@patch('builtins.input', side_effect=mock_input_nine)
+def test_practice_arithmetic_multiplication(mock_input_nine, mock_generate_integer):
+    # Testing practice_arithmetic function for multiplication
+    assert proj1.practice_arithmetic(1, '*') == 10  # Assuming 10 correct answers for level 1
+
+@patch('proj1.generate_integer', side_effect=mock_generate_integer)
+@patch('builtins.input', side_effect=mock_input_one)
+def test_practice_arithmetic_division(mock_input_one, mock_generate_integer):
+    # Testing practice_arithmetic function for division
+    assert proj1.practice_arithmetic(1, '/') == 10  # Assuming 10 correct answers for level 1
