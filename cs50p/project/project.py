51238328cd890da8e585c96af912c2ef468dc7e0
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-26 03:43:26 +0000

    Mon, Feb 26, 2024, 11:43 AM +08:00

--- cs50p/project/project.py
+++ cs50p/project/project.py
@@ -1,3 +1,7 @@
-def main():
+def add_grade():
     """
-    
+    Allows teacher to add a grade for a specific student and assignment/test.
+
+    :param: student_id, test_id, grade
+    :type student_id: int, test_id: int, grade: float
+    :raise ValueError: if student_id not
