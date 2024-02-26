1bcf3ed382b8f34ed9465fa82095417c687cad45
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-26 09:26:07 +0000

    Mon, Feb 26, 2024, 5:26 PM +08:00

--- cs50p/project/test/test_game.py
+++ cs50p/project/test/test_game.py
@@ -13,4 +13,6 @@ def mock_generate_integer(n):
                                       '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'
                                       '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'])
 def test_practice_addition(mock_input, mock_generate_integer):
-    assert game.practice_addition(1) == 10
+    result = game.practice_addition(1)
+    print("Result:", result)
+    assert result == 10
