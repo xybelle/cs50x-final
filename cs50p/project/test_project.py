9623f1ba328b56a7e1a0d20551e669efb9b39b92
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-27 10:04:30 +0000

    Tue, Feb 27, 2024, 6:04 PM +08:00

--- cs50p/project/test_project.py
+++ cs50p/project/test_project.py
@@ -13,6 +13,7 @@ student_list = [
 def test_show_list_empty(capfd, monkeypatch):
     # Mock input to simulate user pressing enter
     monkeypatch.setattr('builtins.input', lambda _: "")
+    monkeypatch.setattr()
     monkeypatch.delitem(project.student_list, "stu_id", raising=False)
     monkeypatch.delitem(project.student_list, "name", raising=False)
     # Call function
