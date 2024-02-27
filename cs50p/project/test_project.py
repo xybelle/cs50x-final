67b7622b9f2e62032482c8e9af804e9935155106
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-27 09:07:00 +0000

    Tue, Feb 27, 2024, 5:07 PM +08:00

--- cs50p/project/test_project.py
+++ cs50p/project/test_project.py
@@ -3,9 +3,18 @@ from project import show_list
 def test_show_list_empty(capfd, monkeypatch):
     # Mock input to simulate user pressing enter
     monkeypatch.setattr('builtins.input', lambda _: "")
-    # Call your function
+    # Call function
     show_list()
     # Capture the output
     out, err = capfd.readouterr()
     # Assert that the output is as expected
     assert "\033[3mNo students added yet\033[0m\n" in out
+
+def test_show_list_not_empty(capfd, monkeypatch):
+    # Mock input to simulate user pressing enter
+    monkeypatch.setattr('builtins.input', lambda _: "")
+    # Call function
+    show_list()
+    out, err = capfd.readouterr()
+    # Assert that the output is as expected
+    assert 
