6ec7692ba91fd7b367cd30a470f3025b606482e1
xybelle <150109343+xybelle@users.noreply.github.com>
2024-02-27 07:58:14 +0000

    Tue, Feb 27, 2024, 3:58 PM +08:00

--- cs50p/project/project.py
+++ cs50p/project/project.py
@@ -53,7 +53,7 @@ def main():
 def show_list():
     """Allows teacher to view student list"""
     print(tabulate(student_list, headers="keys", tablefmt="fancy_outline"))
-    back = input("Press any key to go back to main menu: ")
+    back = input("Press enter to go back to main menu")
     if back:
         return
 
