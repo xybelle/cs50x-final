3a23d9064d23cfcbdb7584bcd3be870ec7e55930
xybelle <150109343+xybelle@users.noreply.github.com>
2024-03-01 08:32:35 +0000

    Fri, Mar 1, 2024, 4:32 PM +08:00

--- cs50p/project/test_project.py
+++ cs50p/project/test_project.py
@@ -4,6 +4,7 @@ import project
 def test_show_list_empty(capfd, monkeypatch):
     # Mock input to simulate user pressing enter
     monkeypatch.setattr("builtins.input", lambda _: "")
+    # Mock student_list to be an empty list
     monkeypatch.setattr("project.student_list", [])
     # Call function
     project.show_list()
@@ -15,6 +16,7 @@ def test_show_list_empty(capfd, monkeypatch):
 def test_show_list_not_empty(capfd, monkeypatch):
     # Mock input to simulate user pressing enter
     monkeypatch.setattr('builtins.input', lambda _: "")
+    # Mock student_list to have an entry
     monkeypatch.setattr("project.student_list", [{"stu_id": "1234", "name": "Harry"}])
     # Call function
     project.show_list()
@@ -27,4 +29,14 @@ def test_show_list_not_empty(capfd, monkeypatch):
 
 def test_add_student_new(capfd, monkeypatch):
     # Mock input to simulate user entering a name and ID
-    monkeypatch.setattr("builtins.input", lambda _: "1234")
+    monkeypatch.setattr("builtins.input", lambda _: "3456")
+    monkeypatch.setattr("builtins.input", lambda _: "Gabbie")
+    # Mock student_list to be an empty list
+    monkeypatch.setattr("project.student_list", [])
+    # Call function
+    project.add_student()
+    # Capture the output
+    out, err = capfd.readouterr()
+    # Assert that the output is as expected
+    assert "\033[3mStudent successfully added\033[0m\n" in out
+
