4ae5aff3a79f9953d8fbdf1a3e3bad301ba08e3c
xybelle <150109343+xybelle@users.noreply.github.com>
2024-03-01 08:48:41 +0000

    Fri, Mar 1, 2024, 4:48 PM +08:00

--- cs50p/project/test_project.py
+++ cs50p/project/test_project.py
@@ -53,4 +53,15 @@ def test_add_student_none(capfd, monkeypatch):
     assert "\033[3mStudent successfully added\033[0m\n" in out
 
 def test_add_student_not_new(capfd, monkeypatch):
-    
+    # Mock input to simuate user entering and id that's already in student_list
+    inputs = iter(["Gabbie", "1234", "Gabbie", "3456"])
+    monkeypatch.setattr("builtins.input", lambda _: next(inputs))
+    # Mock student list
+    monkeypatch.setattr("project.student_list", [{"stu_id": "1234", "name": "Harry"}])
+    # Call function
+    project.add_student()
+    # Capture the output
+    out, err = capfd.readouterr()
+    # Assert that the output is as expected
+    assert "Student ID already in use" in out
+    assert "\033[3mStudent successfully added\033[0m\n" in out
